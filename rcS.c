e0921ed71bab0ad7d2344774a33c22809eb10190
John Crispin <blogic@openwrt.org>
2014-01-29 03:30:57 +0000

    force all runqueue tasks to a timeout of 15s
    Signed-off-by: John Crispin <blogic@openwrt.org>

--- rcS.c
+++ rcS.c
@@ -115,6 +115,7 @@ static void add_initd(struct runqueue *q, char *file, char *param)
 	s = calloc_a(sizeof(*s), &f, strlen(file) + 1, &p, strlen(param) + 1);
 	s->proc.task.type = &initd_type;
 	s->proc.task.complete = q_initd_complete;
+	s->proc.task.run_timeout = 15000;
 	s->param = p;
 	s->file = f;
 	strcpy(s->param, param);
