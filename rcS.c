openwrt/procd

commit d42b21ed9305b91f300f0cebda77e5792f29fc07

procd/rcS: Use /dev/null as stdin
This change ensures that /etc/init.d/* scripts are started with
/dev/null as stdin. This is useful in cases where an init.d script
reads (e.g. by mistake) from stdin, which a user can perceive as if
some characters typed into shell on serial console are "eaten" by
something else (i.e. by the init.d script running on background). This
is very annoying, because each character needs to be pressed several
times before it appears on the screen.

Signed-off-by: Michal Sojka <sojkam1@fel.cvut.cz>

rcS.c
@@ -88,8 +88,14 @@ static void q_initd_run(struct runqueue *q, struct runqueue_task *t)
 		return;
 	}
 	close(pipefd[0]);
+
+	int devnull = open("/dev/null", O_RDONLY);
+	dup2(devnull, STDIN_FILENO);
 	dup2(pipefd[1], STDOUT_FILENO);
 	dup2(pipefd[1], STDERR_FILENO);
+
+	if (devnull > STDERR_FILENO)
+		close(devnull);
 
 	execlp(s->file, s->file, s->param, NULL);
 	exit(1);

commit 992b796204caf5b0290ea4a1246b43b353b6c1d7

rcS: add missing fcntl.h include
Fixes: d42b21ed9305 "procd/rcS: Use /dev/null as stdin"

Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

rcS.c
@@ -24,6 +24,7 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <glob.h>
 
 #include <libubox/ustream.h>