openwrt/procd

commit 7f0f6b2a73f87c31cefc488316e387ed31838ed8

procd: add docker support
detects if running in a docker container, which then requires special
treatment of mounts. OpenWrt within Docker is useful for CI testing.

Signed-off-by: Paul Spooren <mail@aparcar.org>

container.h
@@ -16,9 +16,11 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 static inline bool is_container() {
-	return !!getenv("container");
+	struct stat s;
+	return !!getenv("container") || !!stat("/.dockerenv", &s);
 }
 
 #endif

commit ade00ca585a49c8478bf60eb24ce385676be37a4

container: fix .dockerenv stat check
applied check wasn't evaluating the existence of the file properly.

Fixes: 7f0f6b2a73f8 ("procd: add docker support")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

container.h
@@ -20,7 +20,8 @@
 
 static inline bool is_container() {
 	struct stat s;
-	return !!getenv("container") || !!stat("/.dockerenv", &s);
+	int r = stat("/.dockerenv", &s);
+	return !!getenv("container") || r == 0;
 }
 
 #endif