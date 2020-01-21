openwrt/procd

commit 832369078d818d19ab64051fdc8da9e06c90ad88

state: fix shutdown when running in a container (FS#2425)
Applies patch from @mikma [0] to fix Docker shutdown. Added detection to
state.c if running in a container or not.

Tested with a x86/64 Docker image.

Fixes FS#2425

CC: Mikael Magnusson <mikma@users.sourceforge.net>
CC: Petr Štetiar <ynezz@true.cz>

[0]: https://github.com/mikma/lxd-openwrt/blob/master/patches/procd-master/0003-docker-fix-problem-stopping-container.patch

Signed-off-by: Paul Spooren <mail@aparcar.org>
Signed-off-by: Hans Dedecker <dedeckeh@gmail.com> [coding style aligment]

state.c
@@ -20,6 +20,7 @@
 #include <sys/types.h>
 #include <signal.h>
 
+#include "container.h"
 #include "procd.h"
 #include "syslog.h"
 #include "plug/hotplug.h"
@@ -157,18 +158,22 @@ static void state_enter(void)
 		else
 			LOG("- reboot -\n");
 
-		/* Allow time for last message to reach serial console, etc */
-		sleep(1);
-
-		/* We have to fork here, since the kernel calls do_exit(EXIT_SUCCESS)
-		 * in linux/kernel/sys.c, which can cause the machine to panic when
-		 * the init process exits... */
-		if (!vfork( )) { /* child */
-			reboot(reboot_event);
-			_exit(EXIT_SUCCESS);
-		}
-		while (1)
+		if (!is_container()) {
+			/* Allow time for last message to reach serial console, etc */
 			sleep(1);
+
+			/* We have to fork here, since the kernel calls do_exit(EXIT_SUCCESS)
+			 * in linux/kernel/sys.c, which can cause the machine to panic when
+			 * the init process exits... */
+			if (!vfork( )) { /* child */
+				reboot(reboot_event);
+				_exit(EXIT_SUCCESS);
+			}
+
+			while (1)
+				sleep(1);
+		} else
+			exit(0);
 #else
 		exit(0);
 #endif

commit 856b5f8be04620c785b1af35cc16bc3f6d625a61

state: fix reboot causing shutdown inside LXC container
Executing `reboot` command in OpenWrt system runing inside LXC container
results in a shutdown of the container instead of rebooting the
container.

This appears to have been caused by commit 832369078d81 ("state: fix
shutdown when running in a container (FS#2425)"), which exits the pid
einz instead of the reboot().

While at it, refactor the halting code into separate function to shorten
the switch/case block and make it clearer, decrease the indentation
level by reversing the container if condition, replace magic 0 with
EXIT_SUCCESS constant in exit() and make it wait 1s for reboot message
delivery in both container/host cases as well.

Ref: FS#2666
Cc: Paul Spooren <mail@aparcar.org>
Fixes: 832369078d81 ("state: fix shutdown when running in a container (FS#2425)")
Tested-by: Baptiste Jonglez <lede@bitsofnetworks.org>
Signed-off-by: Petr Štetiar <ynezz@true.cz>

state.c
@@ -94,6 +94,34 @@ static void set_console(void)
 		set_stdio(tty);
 }
 
+static void perform_halt()
+{
+	if (reboot_event == RB_POWER_OFF)
+		LOG("- power down -\n");
+	else
+		LOG("- reboot -\n");
+
+	/* Allow time for last message to reach serial console, etc */
+	sleep(1);
+
+	if (is_container()) {
+		reboot(reboot_event);
+		exit(EXIT_SUCCESS);
+		return;
+	}
+
+	/* We have to fork here, since the kernel calls do_exit(EXIT_SUCCESS)
+	 * in linux/kernel/sys.c, which can cause the machine to panic when
+	 * the init process exits... */
+	if (!vfork()) { /* child */
+		reboot(reboot_event);
+		_exit(EXIT_SUCCESS);
+	}
+
+	while (1)
+		sleep(1);
+}
+
 static void state_enter(void)
 {
 	char ubus_cmd[] = "/sbin/ubusd";
@@ -153,29 +181,9 @@ static void state_enter(void)
 		sync();
 		sleep(1);
 #ifndef DISABLE_INIT
-		if (reboot_event == RB_POWER_OFF)
-			LOG("- power down -\n");
-		else
-			LOG("- reboot -\n");
-
-		if (!is_container()) {
-			/* Allow time for last message to reach serial console, etc */
-			sleep(1);
-
-			/* We have to fork here, since the kernel calls do_exit(EXIT_SUCCESS)
-			 * in linux/kernel/sys.c, which can cause the machine to panic when
-			 * the init process exits... */
-			if (!vfork( )) { /* child */
-				reboot(reboot_event);
-				_exit(EXIT_SUCCESS);
-			}
-
-			while (1)
-				sleep(1);
-		} else
-			exit(0);
+		perform_halt();
 #else
-		exit(0);
+		exit(EXIT_SUCCESS);
 #endif
 		break;
 