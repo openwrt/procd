openwrt/procd

commit 557f11b3a20f2cf6897aad067ffde9d92e9fe015

instance: provide error feedback if ujail binary is missing
Otherwise it's quite hard to track such issues.  While at it, be DRY and
use UJAIL_BIN_PATH constant for ujail binary.

Signed-off-by: Petr Štetiar <ynezz@true.cz>

service/instance.c
@@ -36,6 +36,7 @@
 #include "service.h"
 #include "instance.h"
 
+#define UJAIL_BIN_PATH "/sbin/ujail"
 
 enum {
 	INSTANCE_ATTR_COMMAND,
@@ -205,7 +206,7 @@ jail_run(struct service_instance *in, char **argv)
 	struct jail *jail = &in->jail;
 	int argc = 0;
 
-	argv[argc++] = "/sbin/ujail";
+	argv[argc++] = UJAIL_BIN_PATH;
 
 	if (jail->name) {
 		argv[argc++] = "-n";
@@ -795,9 +796,13 @@ instance_jail_parse(struct service_instance *in, struct blob_attr *attr)
 	struct blob_attr *tb[__JAIL_ATTR_MAX];
 	struct jail *jail = &in->jail;
 	struct stat s;
+	int r;
 
-	if (stat("/sbin/ujail", &s))
+	r = stat(UJAIL_BIN_PATH, &s);
+	if (r < 0) {
+		ERROR("unable to find %s: %m (%d)\n", UJAIL_BIN_PATH, r);
 		return 0;
+	}
 
 	blobmsg_parse(jail_attr, __JAIL_ATTR_MAX, tb,
 		blobmsg_data(attr), blobmsg_data_len(attr));

commit 09b9bd828981a4f9271f8906f7b6f5af04e1a6f9

instance: turn error into debug message for missing ujail binary
Since commit 557f11b3a20f ("instance: provide error feedback if ujail
binary is missing") worrying log spam of the form "unable to find
/sbin/jail ..." may be encountered.

This corresponds with the changes done in the upstream commit
bcb86554f1b4 ("instance: add 'requirejail' attribute").

Fixes: 557f11b3a20f ("instance: provide error feedback if ujail binary is missing")
Ref: https://forum.openwrt.org/t/openwrt-19-07-2-service-release/57066
Signed-off-by: Petr Štetiar <ynezz@true.cz>

service/instance.c
@@ -800,7 +800,7 @@ instance_jail_parse(struct service_instance *in, struct blob_attr *attr)
 
 	r = stat(UJAIL_BIN_PATH, &s);
 	if (r < 0) {
-		ERROR("unable to find %s: %m (%d)\n", UJAIL_BIN_PATH, r);
+		DEBUG(2, "unable to find %s: %m (%d)\n", UJAIL_BIN_PATH, r);
 		return 0;
 	}
 