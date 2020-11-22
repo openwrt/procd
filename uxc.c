openwrt/procd

commit 04a2eddcb1a33fee865b8f09100567bd54d36be7

uxc: make force-delete kill container process
Don't allow to delete running containers unless '--force' is
specified. If '--force' is specified, send KILL signal to container
process before deleting it.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

uxc.c
@@ -636,7 +636,7 @@ static int uxc_boot(void)
 	return ret;
 }
 
-static int uxc_delete(char *name)
+static int uxc_delete(char *name, bool force)
 {
 	struct blob_attr *cur, *tb[__CONF_MAX];
 	int rem, ret = 0;
@@ -662,6 +662,18 @@ static int uxc_delete(char *name)
 
 	if (!found)
 		return ENOENT;
+
+	if (s && s->running) {
+		if (force) {
+			ret = uxc_kill(name, SIGKILL));
+			if (ret)
+				goto errout;
+
+		} else {
+			ret = EWOULDBLOCK;
+			goto errout;
+		}
+	}
 
 	if (stat(fname, &sb) == -1) {
 		ret=ENOENT;
@@ -823,7 +835,7 @@ int main(int argc, char **argv)
 			if (optind != argc - 2)
 				goto usage_out;
 
-			ret = uxc_delete(argv[optind + 1]);
+			ret = uxc_delete(argv[optind + 1], force);
 			break;
 
 		case CMD_CREATE:

commit df7fa7bae605e865c5ab2b80461a4ccbbbfc1fab

uxc: fix incomplete commit
Fixes: 04a2edd ("uxc: make force-delete kill container process")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

uxc.c
@@ -639,6 +639,7 @@ static int uxc_boot(void)
 static int uxc_delete(char *name, bool force)
 {
 	struct blob_attr *cur, *tb[__CONF_MAX];
+	struct runtime_state *s = NULL;
 	int rem, ret = 0;
 	bool found = false;
 	char *fname;
@@ -663,9 +664,11 @@ static int uxc_delete(char *name, bool force)
 	if (!found)
 		return ENOENT;
 
+	s = avl_find_element(&runtime, name, s, avl);
+
 	if (s && s->running) {
 		if (force) {
-			ret = uxc_kill(name, SIGKILL));
+			ret = uxc_kill(name, SIGKILL);
 			if (ret)
 				goto errout;
 