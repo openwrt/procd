openwrt/procd

commit 51f1cd2366ebfc1f4ebbbfc819d36086ed3b5eb9

trace: free string returned by blobmsg_format_json_indent()
Coverity CID: 1446205 Resource leak

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

trace/trace.c
@@ -117,6 +117,7 @@ static void print_syscalls(int policy, const char *json)
 {
 	void *c, *d, *e;
 	int i;
+	char *tmp;
 
 	if (mode == UTRACE) {
 		set_syscall("rt_sigaction", 1);
@@ -168,8 +169,12 @@ static void print_syscalls(int policy, const char *json)
 			ULOG_ERR("failed to open %s\n", json);
 		}
 	} else {
-		printf("%s\n",
-			blobmsg_format_json_indent(b.head, true, 0));
+		tmp = blobmsg_format_json_indent(b.head, true, 0);
+		if (!tmp)
+			return;
+
+		printf("%s\n", tmp);
+		free(tmp);
 	}
 
 }

commit e5b38fd1298cec50f68b01568fcecf5e626cd286

trace: free memory allocated by blobmsg_format_json_indent()
Only one out of two occurances have been addressed previously.
Close the other one now.

Coverity CID: 1446205
Fixes: 51f1cd23 ("trace: free string returned by blobmsg_format_json_indent()")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

trace/trace.c
@@ -162,7 +162,12 @@ static void print_syscalls(int policy, const char *json)
 	if (json) {
 		FILE *fp = fopen(json, "w");
 		if (fp) {
-			fprintf(fp, "%s\n", blobmsg_format_json_indent(b.head, true, 0));
+			tmp = blobmsg_format_json_indent(b.head, true, 0);
+			if (!tmp)
+				return;
+
+			free(tmp);
+			fprintf(fp, "%s\n", tmp);
 			fclose(fp);
 			ULOG_INFO("saving syscall trace to %s\n", json);
 		} else {
@@ -176,7 +181,6 @@ static void print_syscalls(int policy, const char *json)
 		printf("%s\n", tmp);
 		free(tmp);
 	}
-
 }
 
 static void report_seccomp_vialation(pid_t pid, unsigned syscall)