openwrt/procd

commit e5b38fd1298cec50f68b01568fcecf5e626cd286

trace: free memory allocated by blobmsg_format_json_indent()
Only one out of two occurances have been addressed previously.
Close the other one now.

Coverity CID: 1446205
Fixes: 51f1cd23 ("trace: free string returned by blobmsg_format_json_indent()")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

trace/trace.c
@@ -162,7 +162,12 @@ static void print_syscalls(int policy, const char *json)
 	if (json) {
 		FILE *fp = fopen(json, "w");
 		if (fp) {
-			fprintf(fp, "%s\n", blobmsg_format_json_indent(b.head, true, 0));
+			tmp = blobmsg_format_json_indent(b.head, true, 0);
+			if (!tmp)
+				return;
+
+			free(tmp);
+			fprintf(fp, "%s\n", tmp);
 			fclose(fp);
 			ULOG_INFO("saving syscall trace to %s\n", json);
 		} else {
@@ -176,7 +181,6 @@ static void print_syscalls(int policy, const char *json)
 		printf("%s\n", tmp);
 		free(tmp);
 	}
-
 }
 
 static void report_seccomp_vialation(pid_t pid, unsigned syscall)

commit 96d8bf2c7b6773636efb8b175993e5d17ff7d0bf

trace: fix potential use-after-free occurence
char* tmp is used in the fprintf function altough it is already freed.

Fixes: e5b38fd1 ("trace: free memory allocated by blobmsg_format_json_indent()")

Signed-off-by: Nick Hainke <vincent@systemli.org>

trace/trace.c
@@ -166,8 +166,8 @@ static void print_syscalls(int policy, const char *json)
 			if (!tmp)
 				return;
 
-			free(tmp);
 			fprintf(fp, "%s\n", tmp);
+			free(tmp);
 			fclose(fp);
 			ULOG_INFO("saving syscall trace to %s\n", json);
 		} else {