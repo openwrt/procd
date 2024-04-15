openwrt/procd

commit 91da63d3d3fd680c805dd1a1b78df5b8731a8173

properly handle return codes
Signed-off-by: John Crispin <blogic@openwrt.org>

state.c
@@ -43,12 +43,14 @@ static int reboot_event;
 
 static void set_stdio(const char* tty)
 {
-	chdir("/dev");
-	freopen(tty, "r", stdin);
-	freopen(tty, "w", stdout);
-	freopen(tty, "w", stderr);
-	chdir("/");
-	fcntl(STDERR_FILENO, F_SETFL, fcntl(STDERR_FILENO, F_GETFL) | O_NONBLOCK);
+	if (chdir("/dev") ||
+	    !freopen(tty, "r", stdin) ||
+	    !freopen(tty, "w", stdout) ||
+	    !freopen(tty, "w", stderr) ||
+	    chdir("/"))
+		ERROR("failed to set stdio\n");
+	else
+		fcntl(STDERR_FILENO, F_SETFL, fcntl(STDERR_FILENO, F_GETFL) | O_NONBLOCK);
 }
 
 static void set_console(void)
@@ -70,7 +72,10 @@ static void set_console(void)
 		i++;
 	}
 
-	chdir("/dev");
+	if (chdir("/dev")) {
+		ERROR("failed to change dir to /dev\n");
+		return;
+	}
 	while (tty!=NULL) {
 		f = open(tty, O_RDONLY);
 		if (f >= 0) {
@@ -81,7 +86,8 @@ static void set_console(void)
 		tty=try[i];
 		i++;
 	}
-	chdir("/");
+	if (chdir("/"))
+		ERROR("failed to change dir to /\n");
 
 	if (tty != NULL)
 		set_stdio(tty);

initd/early.c
@@ -77,7 +77,8 @@ early_mounts(void)
 	mkdir("/tmp/run", 0777);
 	mkdir("/tmp/lock", 0777);
 	mkdir("/tmp/state", 0777);
-	symlink("/tmp", "/var");
+	if (symlink("/tmp", "/var"))
+		ERROR("failed to symlink /tmp -> /var\n");
 }
 
 static void

initd/mkdev.c
@@ -121,7 +121,5 @@ int mkdev(const char *name, int _mode)
 	n_patterns = 1;
 	find_devs(true);
 	find_devs(false);
-	chdir("/");
-
-	return 0;
+	return chdir("/");
 }

initd/preinit.c
@@ -38,7 +38,8 @@ check_dbglvl(void)
 
 	if (!fp)
 		return;
-	fscanf(fp, "%d", &lvl);
+	if (fscanf(fp, "%d", &lvl) == EOF)
+		ERROR("failed to read debug level\n");
 	fclose(fp);
 	unlink("/tmp/debug_level");
 

inittab.c
@@ -70,9 +70,11 @@ static int dev_open(const char *dev)
 	int fd = -1;
 
 	if (dev) {
-		chdir("/dev");
-		fd = open( dev, O_RDWR);
-		chdir("/");
+		if (chdir("/dev"))
+			ERROR("failed to change dir to /dev\n");
+		fd = open(dev, O_RDWR);
+		if (chdir("/"))
+			ERROR("failed to change dir to /\n");
 	}
 
 	return fd;
@@ -83,9 +85,8 @@ static int dev_exist(const char *dev)
 	int res;
 
 	res = dev_open(dev);
-	if (res != -1) {
+	if (res != -1)
 		close(res);
-	}
 
 	return (res != -1);
 }

jail/jail.c
@@ -313,12 +313,16 @@ static int spawn_child(void *arg)
 			sysfs = 1;
 			break;
 		case 'n':
-			sethostname(optarg, strlen(optarg));
+			if (sethostname(optarg, strlen(optarg)))
+				ERROR("failed to sethostname: %s\n", strerror(errno));
 			break;
 		}
 	}
 
-	asprintf(&mpoint, "%s/old", path);
+	if (asprintf(&mpoint, "%s/old", path) < 0) {
+		ERROR("failed to alloc pivot path: %s\n", strerror(errno));
+		return -1;
+	}
 	mkdir_p(mpoint, 0755);
 	if (pivot_root(path, mpoint) == -1) {
 		ERROR("pivot_root failed:%s\n", strerror(errno));
@@ -370,13 +374,17 @@ static void spawn_namespace(const char *path, int argc, char **argv)
 	char *dir = get_current_dir_name();
 
 	uloop_init();
-	chdir(path);
+	if (chdir(path)) {
+		ERROR("failed to chdir() into the jail\n");
+		return;
+	}
 	namespace_process.pid = clone(spawn_child,
 			child_stack + STACK_SIZE,
 			CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, argv);
 
 	if (namespace_process.pid != -1) {
-		chdir(dir);
+		if (chdir(dir))
+			ERROR("failed to chdir() out of the jail\n");
 		free(dir);
 		uloop_process_add(&namespace_process);
 		uloop_run();

plug/hotplug.c
@@ -198,7 +198,10 @@ send_to_kernel:
 		ERROR("Failed to open %s\n", loadpath);
 		exit(-1);
 	}
-	write(load, "1", 1);
+	if (write(load, "1", 1) == -1) {
+		ERROR("Failed to write to %s\n", loadpath);
+		exit(-1);
+	}
 	close(load);
 
 	snprintf(syspath, sizeof(syspath), "/sys/%s/data", dev);
@@ -214,7 +217,10 @@ send_to_kernel:
 		if (len <= 0)
 			break;
 
-		write(fw, buf, len);
+		if (write(fw, buf, len) == -1) {
+			ERROR("failed to write firmware file %s/%s to %s\n", dir, file, dev);
+			break;
+		}
 	}
 
 	if (src >= 0)
@@ -222,7 +228,8 @@ send_to_kernel:
 	close(fw);
 
 	load = open(loadpath, O_WRONLY);
-	write(load, "0", 1);
+	if (write(load, "0", 1) == -1)
+		ERROR("failed to write to %s\n", loadpath);
 	close(load);
 
 	DEBUG(2, "Done loading %s\n", path);

service/instance.c
@@ -283,8 +283,10 @@ instance_run(struct service_instance *in, int _stdout, int _stderr)
 	}
 
 	if (in->uid || in->gid) {
-		setuid(in->uid);
-		setgid(in->gid);
+		if (setuid(in->uid) || setgid(in->gid)) {
+			ERROR("failed to set uid:%d, gid:%d\n", in->uid, in->gid);
+			exit(127);
+		}
 	}
 	execvp(argv[0], argv);
 	exit(127);

commit 706346141eef92200361b50c46a759af9ad763c7

state: set_stdio: chdir back to / in case of failure
set_stdio chdirs to /dev/ to facilitate easy freopen of the console
device name given by the tty parameter. Make sure to chdir back to / in
all cases, even in the error path. This keeps the function free from
unintended side effects.

Before this commit, in case of an error, the working directory would
remain /dev/ which would break sysupgrade because the rest of the code
would rely on the current working directory to be unchanged, which is
not an unreasonable expectation to make.

Fixing this fixes an issue where sysupgrade would fail, when
/dev/console does not exist or cannot be opened, which can happen for
example when setting console= on kernel cmdline.

Closes: https://github.com/openwrt/openwrt/issues/6005
Fixes: 91da63d3d3fd ("properly handle return codes")
Signed-off-by: Andreas Gnau <andreas.gnau@iopsys.eu>

state.c
@@ -48,11 +48,12 @@ static void set_stdio(const char* tty)
 	if (chdir("/dev") ||
 	    !freopen(tty, "r", stdin) ||
 	    !freopen(tty, "w", stdout) ||
-	    !freopen(tty, "w", stderr) ||
-	    chdir("/"))
+	    !freopen(tty, "w", stderr))
 		ERROR("failed to set stdio: %m\n");
 	else
 		fcntl(STDERR_FILENO, F_SETFL, fcntl(STDERR_FILENO, F_GETFL) | O_NONBLOCK);
+	if (chdir("/"))
+		ERROR("failed to change dir to /: %m\n");
 }
 
 static void set_console(void)