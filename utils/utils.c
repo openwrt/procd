openwrt/procd

commit 6f52ef3cc9f01e952e7f32d10c7f8612e635e80c

get_cmdline_val: search for entire name, not just suffix
also fix writing of 0-byte to buffer

Signed-off-by: Steven Barth <steven@midlink.org>

utils/utils.c
@@ -129,36 +129,27 @@ blobmsg_list_equal(struct blobmsg_list *l1, struct blobmsg_list *l2)
 
 char* get_cmdline_val(const char* name, char* out, int len)
 {
-	char pattern[CMDLINE_SIZE + 1];
-	char line[CMDLINE_SIZE + 1];
-	char *res = NULL, *tty;
-	int r, fd;
-	regex_t pat_cmdline;
-	regmatch_t matches[2];
-
-	fd = open("/proc/cmdline", O_RDONLY);
-	if (fd < 0)
-		return NULL;
-
-	r = read(fd, line, CMDLINE_SIZE);
-	if ( r <= 0 ) {
-		close(fd);
-		return NULL;
-	}
-	line[r] = '\0';
+	char line[CMDLINE_SIZE + 1], *c, *sptr;
+	int fd = open("/proc/cmdline", O_RDONLY);
+	ssize_t r = read(fd, line, sizeof(line) - 1);
 	close(fd);
 
-	sprintf( pattern, "%s=([^ \n]*)", name);
-	regcomp(&pat_cmdline, pattern, REG_EXTENDED);
-	if (!regexec(&pat_cmdline, line, 2, matches, 0)) {
-		line[matches[1].rm_eo] = '\0';
-		tty = (line + matches[1].rm_so);
-		strncpy(out, tty, len);
-		tty[len-1] = '\0';
-		res = out;
+	if (r <= 0)
+		return NULL;
+
+	line[r] = 0;
+
+	for (c = strtok_r(line, " \t\n", &sptr); c;
+			c = strtok_r(NULL, " \t\n", &sptr)) {
+		char *sep = strchr(c, '=');
+		ssize_t klen = sep - c;
+		if (klen < 0 || strncmp(name, c, klen) || name[klen] != 0)
+			continue;
+
+		strncpy(out, &sep[1], len);
+		out[len-1] = 0;
+		return out;
 	}
 
-	regfree(&pat_cmdline);
-
-	return res;
+	return NULL;
 }

commit 1e411a58f52ea49e5cb4ef4a067ecdab8befd55c

utils: get_cmdline_val: add missing error check
Add check to prevent read() in case open() has failed.

Coverity CID: 1595922: Error handling issues  (NEGATIVE_RETURNS)
Fixes: 6f52ef3 ("get_cmdline_val: search for entire name, not just suffix")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

utils/utils.c
@@ -167,7 +167,12 @@ char *get_cmdline_val_offset(const char *name, char *out, int len, int offset)
 {
 	char line[CMDLINE_SIZE + 1], *c, *sptr;
 	int i, fd = open("/proc/cmdline", O_RDONLY);
-	ssize_t r = read(fd, line, sizeof(line) - 1);
+	ssize_t r;
+
+	if (fd < 0)
+		return NULL;
+
+	r = read(fd, line, sizeof(line) - 1);
 	close(fd);
 
 	if (r <= 0)