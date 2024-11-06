openwrt/procd

commit 0f88a52dc8a5451bd7f1f33ffff3b1ba3db98174

system: replace openwrt_release with os-release
The information required for output via the ubus is currently read from
the file '/etc/openwrt_release'. The same information can also be found
under '/usr/lib/os-release'. This file contains further information and is
also the most common used file in other Linux distributions.

So let´s use this file.

Signed-off-by: Florian Eckert <fe@dev.tdt.de>

system.c
@@ -251,7 +251,7 @@ static int system_board(struct ubus_context *ctx, struct ubus_object *obj,
 	if (rootfs_type)
 		blobmsg_add_string(&b, "rootfs_type", rootfs_type);
 
-	if ((f = fopen("/etc/openwrt_release", "r")) != NULL)
+	if ((f = fopen("/usr/lib/os-release", "r")) != NULL)
 	{
 		c = blobmsg_open_table(&b, "release");
 
@@ -267,17 +267,17 @@ static int system_board(struct ubus_context *ctx, struct ubus_object *obj,
 
 			*(val++) = 0;
 
-			if (!strcasecmp(key, "DISTRIB_ID"))
+			if (!strcasecmp(key, "NAME"))
 				key = "distribution";
-			else if (!strcasecmp(key, "DISTRIB_RELEASE"))
+			else if (!strcasecmp(key, "VERSION"))
 				key = "version";
-			else if (!strcasecmp(key, "DISTRIB_REVISION"))
+			else if (!strcasecmp(key, "BUILD_ID"))
 				key = "revision";
-			else if (!strcasecmp(key, "DISTRIB_CODENAME"))
+			else if (!strcasecmp(key, "VERSION_CODENAME"))
 				key = "codename";
-			else if (!strcasecmp(key, "DISTRIB_TARGET"))
+			else if (!strcasecmp(key, "OPENWRT_BOARD"))
 				key = "target";
-			else if (!strcasecmp(key, "DISTRIB_DESCRIPTION"))
+			else if (!strcasecmp(key, "PRETTY_NAME"))
 				key = "description";
 			else
 				continue;

commit 109fa41b2321506280397e03757976c468832668

system: fix description value from os-release
When changing the read file from '/etc/openwrt_release' to
'/usr/lib/os-release', the correct variable for reading the description
was not selected. The correct variable from the file ‘/usr/lib/os-release’
is 'OPENWRT_RELEASE'.

Fixes: 0f88a52dc8a5 ("system: replace openwrt_release with os-release")
Signed-off-by: Florian Eckert <fe@dev.tdt.de>
[ add fixes tag ]
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

system.c
@@ -277,7 +277,7 @@ static int system_board(struct ubus_context *ctx, struct ubus_object *obj,
 				key = "codename";
 			else if (!strcasecmp(key, "OPENWRT_BOARD"))
 				key = "target";
-			else if (!strcasecmp(key, "PRETTY_NAME"))
+			else if (!strcasecmp(key, "OPENWRT_RELEASE"))
 				key = "description";
 			else if (!strcasecmp(key, "OPENWRT_BUILD_DATE"))
 				key = "builddate";