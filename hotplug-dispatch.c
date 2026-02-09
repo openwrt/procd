openwrt/procd

commit c4e9859876d5db7d8d69e8dc92bc7fde44ee96ec

hotplug-dispatch: use stat if d_type is DT_UNKNOWN
As documented in the readdir man page, support for d_type varies by
filesystem.  The ISO 9660 filesystem used in the x86 target's bootable
ISO images always sets d_type to DT_UNKNOWN.  Fall back to checking
S_ISDIR(st_mode) when d_type is DT_UNKNOWN.

Fixes: 08938fe ("procd: add hotplug-call dispatcher")
Signed-off-by: Jeffrey Bosboom <jbosboom@jeffreybosboom.com>

hotplug-dispatch.c
@@ -390,6 +390,7 @@ static int init_subsystems(void)
 {
 	DIR *dir;
 	struct dirent *dirent;
+	struct stat st;
 
 	add_subsystem("button");
 
@@ -399,7 +400,11 @@ static int init_subsystems(void)
 
 	while ((dirent = readdir(dir))) {
 		/* skip everything but directories */
-		if (dirent->d_type != DT_DIR)
+		if (dirent->d_type == DT_UNKNOWN) {
+			if ((fstatat(dirfd(dir), dirent->d_name, &st, AT_SYMLINK_NOFOLLOW) == -1)
+			    || !S_ISDIR(st.st_mode))
+				continue;
+		} else if (dirent->d_type != DT_DIR)
 			continue;
 
 		/* skip '.' and '..' as well as hidden files */

commit 6b10c71c19b41e1052ddc0755f749a38dab871f7

hotplug-dispatch: fix missing header include
Flag AT_SYMLINK_NOFOLLOW is defined in header fcntl.h which needs to be
included to not break the build with glibc.

Fixes: c4e9859 ("hotplug-dispatch: use stat if d_type is DT_UNKNOWN")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

hotplug-dispatch.c
@@ -18,6 +18,7 @@
 
 #include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <glob.h>
 #include <limits.h>
 #include <stdbool.h>