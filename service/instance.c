openwrt/procd

commit bb95fe8df7115ab57792369c768927eafc042161

jail: make sure jailed process is terminated
Don't ever send SIGKILL to ujail, as that will kill ujail but not the
jailed process.
Instead, let ujail send SIGKILL in case of SIGTERM not succeeding after
the term_timeout which procd now passes down to ujail.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

service/instance.c
@@ -288,11 +288,16 @@ instance_gen_setns_argstr(struct blob_attr *attr)
 static inline int
 jail_run(struct service_instance *in, char **argv)
 {
+	char *term_timeout_str;
 	struct blobmsg_list_node *var;
 	struct jail *jail = &in->jail;
 	int argc = 0;
 
 	argv[argc++] = UJAIL_BIN_PATH;
+
+	asprintf(&term_timeout_str, "%d", in->term_timeout);
+	argv[argc++] = "-t";
+	argv[argc++] = term_timeout_str;
 
 	if (jail->name) {
 		argv[argc++] = "-n";
@@ -867,7 +872,8 @@ instance_stop(struct service_instance *in, bool halt)
 	in->halt = halt;
 	in->restart = in->respawn = false;
 	kill(in->proc.pid, SIGTERM);
-	uloop_timeout_set(&in->timeout, in->term_timeout * 1000);
+	if (!in->has_jail)
+		uloop_timeout_set(&in->timeout, in->term_timeout * 1000);
 }
 
 static void
@@ -884,7 +890,8 @@ instance_restart(struct service_instance *in)
 	in->halt = true;
 	in->restart = true;
 	kill(in->proc.pid, SIGTERM);
-	uloop_timeout_set(&in->timeout, in->term_timeout * 1000);
+	if (!in->has_jail)
+		uloop_timeout_set(&in->timeout, in->term_timeout * 1000);
 }
 
 static void
@@ -1147,7 +1154,7 @@ instance_jail_parse(struct service_instance *in, struct blob_attr *attr)
 	blobmsg_parse(jail_attr, __JAIL_ATTR_MAX, tb,
 		blobmsg_data(attr), blobmsg_data_len(attr));
 
-	jail->argc = 2;
+	jail->argc = 4;
 
 	if (tb[JAIL_ATTR_REQUIREJAIL] && blobmsg_get_bool(tb[JAIL_ATTR_REQUIREJAIL])) {
 		in->require_jail = true;

jail/jail.c
@@ -69,7 +69,7 @@
 #endif
 
 #define STACK_SIZE	(1024 * 1024)
-#define OPT_ARGS	"cC:d:e:EfFG:h:ij:J:ln:NoO:pP:r:R:sS:uU:w:T:y"
+#define OPT_ARGS	"cC:d:e:EfFG:h:ij:J:ln:NoO:pP:r:R:sS:uU:w:t:T:y"
 
 #define OCI_VERSION_STRING "1.0.2"
 
@@ -153,6 +153,7 @@ static struct {
 	char *ocibundle;
 	bool immediately;
 	struct blob_attr *annotations;
+	int term_timeout;
 } opts;
 
 static struct blob_buf ocibuf;
@@ -1093,11 +1094,17 @@ static void jail_handle_signal(int signo)
 	if (hook_running) {
 		DEBUG("forwarding signal %d to the hook process\n", signo);
 		kill(hook_process.pid, signo);
+		/* set timeout to send SIGKILL hook process in case SIGTERM doesn't succeed */
+		if (signo == SIGTERM)
+			uloop_timeout_set(&hook_process_timeout, opts.term_timeout * 1000);
 	}
 
 	if (jail_running) {
 		DEBUG("forwarding signal %d to the jailed process\n", signo);
 		kill(jail_process.pid, signo);
+		/* set timeout to send SIGKILL jail process in case SIGTERM doesn't succeed */
+		if (signo == SIGTERM)
+			uloop_timeout_set(&jail_process_timeout, opts.term_timeout * 1000);
 	}
 }
 
@@ -1112,7 +1119,7 @@ static void signals_init(void)
 
 		if (!sigismember(&sigmask, i))
 			continue;
-		if ((i == SIGCHLD) || (i == SIGPIPE) || (i == SIGSEGV))
+		if ((i == SIGCHLD) || (i == SIGPIPE) || (i == SIGSEGV) || (i == SIGSTOP) || (i == SIGKILL))
 			continue;
 
 		s.sa_handler = jail_handle_signal;
@@ -2577,6 +2584,9 @@ int main(int argc, char **argv)
 	opts.setns.time = -1;
 #endif
 
+	/* default 5 seconds timeout after SIGTERM before SIGKILL is sent */
+	opts.term_timeout = 5;
+
 	umask(022);
 	mount_list_init();
 	init_library_search();
@@ -2673,6 +2683,9 @@ int main(int argc, char **argv)
 			break;
 		case 'O':
 			opts.overlaydir = realpath(optarg, NULL);
+			break;
+		case 't':
+			opts.term_timeout = atoi(optarg);
 			break;
 		case 'T':
 			opts.tmpoverlaysize = optarg;

commit 58ac2a85118e209ca41c3e79de647ca924815890

instance: kill the cgroup when a jailed instance exits
procd supervises ujail, never the jailed payload, so the only thing it
ever learns is ujail's exit status. A jailed instance also has no
bounded stop of its own: instance_stop() and instance_restart() leave
the term timeout unarmed and rely on ujail to terminate the payload,
and ujail's parent death signal stops covering that payload as soon as
it changes its own credentials, because commit_creds() zeroes
pdeath_signal on any uid or gid change. A payload that outlives its
ujail is invisible to procd and collides with the next generation over
whatever it still holds open. Kill the instance cgroup in
instance_exit(), reusing the cgroup.kill write the teardown path
already performs, so that anything left behind the jail is gone before
a new generation starts.

Fixes: bb95fe8df711 ("jail: make sure jailed process is terminated")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

service/instance.c
@@ -660,10 +660,9 @@ instance_add_cgroup(const char *service, const char *instance)
 }
 
 static void
-instance_remove_cgroup(const char *service, const char *instance)
+instance_kill_cgroup(const char *service, const char *instance)
 {
 	char cgnamebuf[256];
-	char *sep;
 	int fd, ret;
 
 	ret = snprintf(cgnamebuf, sizeof(cgnamebuf), "%s/%s/%s/cgroup.kill",
@@ -672,15 +671,30 @@ instance_remove_cgroup(const char *service, const char *instance)
 		return;
 
 	fd = open(cgnamebuf, O_WRONLY);
-	if (fd >= 0) {
-		if (write(fd, "1", 1) < 0)
-			ret = -1;
-		close(fd);
-	}
+	if (fd < 0)
+		return;
 
-	sep = strrchr(cgnamebuf, '/');
-	if (sep)
-		*sep = '\0';
+	if (write(fd, "1", 1) < 0)
+		ULOG_WARN("failed to kill cgroup of %s::%s: %m\n", service,
+			  instance);
+
+	close(fd);
+}
+
+static void
+instance_remove_cgroup(const char *service, const char *instance)
+{
+	char cgnamebuf[256];
+	char *sep;
+	int ret;
+
+	instance_kill_cgroup(service, instance);
+
+	ret = snprintf(cgnamebuf, sizeof(cgnamebuf), "%s/%s/%s", CGROUP_BASEDIR,
+		       service, instance);
+	if (ret >= (int)sizeof(cgnamebuf))
+		return;
+
 	(void)rmdir(cgnamebuf);
 
 	sep = strrchr(cgnamebuf, '/');
@@ -952,6 +966,10 @@ instance_exit(struct uloop_process *p, int ret)
 	in->exit_code = instance_exit_code(ret);
 	uloop_timeout_cancel(&in->timeout);
 	uloop_timeout_cancel(&in->watchdog.timeout);
+
+	if (in->has_jail)
+		instance_kill_cgroup(in->srv->name, in->name);
+
 	service_event("instance.stop", in->srv->name, in->name);
 
 	if (in->halt) {