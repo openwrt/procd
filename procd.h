openwrt/procd

commit f1bf99b09f664dce09f55e7102ce8e6ef77ef10a

add signal handler

signal.c
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Felix Fietkau <nbd@openwrt.org>
+ * Copyright (C) 2013 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <sys/reboot.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include "procd.h"
+
+static void signal_shutdown(int signal, siginfo_t *siginfo, void *data)
+{
+	int event = 0;
+	char *msg = NULL;
+
+	switch(signal) {
+	case SIGTERM:
+		event = RB_AUTOBOOT;
+		msg = "reboot";
+		break;
+	case SIGUSR1:
+	case SIGUSR2:
+		event = RB_POWER_OFF;
+		msg = "poweroff";
+		break;
+	}
+
+	DEBUG(1, "Triggering %s\n", msg);
+	if (event)
+		procd_shutdown(event);
+}
+
+struct sigaction sa_shutdown = {
+	.sa_sigaction = signal_shutdown,
+	.sa_flags = SA_SIGINFO
+};
+
+static void signal_crash(int signal, siginfo_t *siginfo, void *data)
+{
+	ERROR("Rebooting as procd has crashed\n");
+	fflush(stderr);
+	sync();
+	sleep(1);
+	reboot(RB_AUTOBOOT);
+	while (1)
+		;
+}
+
+struct sigaction sa_crash = {
+	.sa_sigaction = signal_crash,
+	.sa_flags = SA_SIGINFO
+};
+
+static void signal_dummy(int signal, siginfo_t *siginfo, void *data)
+{
+	ERROR("Got unexpected signal %d\n", signal);
+}
+
+struct sigaction sa_dummy = {
+	.sa_sigaction = signal_dummy,
+	.sa_flags = SA_SIGINFO
+};
+
+void procd_signal(void)
+{
+	sigaction(SIGPIPE, &sa_dummy, NULL);
+	if (getpid() != 1)
+		return;
+	sigaction(SIGTERM, &sa_shutdown, NULL);
+	sigaction(SIGUSR1, &sa_shutdown, NULL);
+	sigaction(SIGUSR2, &sa_shutdown, NULL);
+	sigaction(SIGSEGV, &sa_crash, NULL);
+	sigaction(SIGBUS, &sa_crash, NULL);
+	sigaction(SIGHUP, &sa_dummy, NULL);
+	sigaction(SIGKILL, &sa_dummy, NULL);
+	sigaction(SIGSTOP, &sa_dummy, NULL);
+}

commit e2010df98d5ef93f0046f3f78bec0982dfdfbb6d

signal, instance: don't run procd's handlers after fork
Signal dispositions survive fork() and are reset only by exec(), so the
child that instance_start() creates keeps procd's own handlers until
execvp() succeeds. A SIGSEGV or SIGBUS in that window reaches
signal_crash(), which reboots the machine, and a SIGTERM reaches
signal_shutdown(), which runs the shutdown state in a process that is
not procd and then execs the service anyway. in->proc.pid is recorded
the moment fork() returns, so instance_stop() and the service signal
method can both aim at the child while it is still pre-exec. Restore
the default dispositions as the child's first act, which is what
execvp() would do moments later.

Fixes: f1bf99b09f66 ("add signal handler")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

procd.h
@@ -51,6 +51,7 @@ void procd_shutdown(int event);
 void procd_early(void);
 void procd_preinit(void);
 void procd_signal(void);
+void procd_signal_reset(void);
 void procd_signal_preinit(void);
 void procd_inittab(void);
 void procd_inittab_run(const char *action);

service/instance.c
@@ -780,6 +780,7 @@ instance_start(struct service_instance *in)
 		return;
 
 	if (!pid) {
+		procd_signal_reset();
 		uloop_done();
 		closefd(opipe[0]);
 		closefd(epipe[0]);

signal.c
@@ -104,3 +104,14 @@ void procd_signal(void)
 	reboot(RB_DISABLE_CAD);
 #endif
 }
+
+void procd_signal_reset(void)
+{
+	struct sigaction s = { 0 };
+	int i;
+
+	s.sa_handler = SIG_DFL;
+
+	for (i = 1; i < _NSIG; i++)
+		sigaction(i, &s, NULL);
+}