openwrt/procd

commit f4d512d93a00a701c9604376e759ec60960d4dfb

jail: place container init and exec into the target cgroup via clone3
Split the monolithic cgroups_apply() into cgroups_create(), which makes
the directory and enables the requested controllers in subtree_control,
cgroups_configure(), which writes the limits and attaches the eBPF
programs, and cgroups_attach_pid(), which joins a task. This lets the
parent create and configure the cgroup before the child exists and attach
init and later exec processes individually rather than as a side effect.

Add cgroups_destroy() to kill stragglers and rmdir the per-container and
parent directories, leaving the shared base intact. Signalling now goes
through a pidfd so a recycled pid cannot be hit; the kill path maps EBADF
accordingly. procd's instance teardown gains instance_remove_cgroup() so
a vanished instance's cgroup is killed and reaped rather than leaked.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

service/instance.c
@@ -595,6 +595,36 @@ instance_add_cgroup(const char *service, const char *instance)
 }
 
 static void
+instance_remove_cgroup(const char *service, const char *instance)
+{
+	char cgnamebuf[256];
+	char *sep;
+	int fd, ret;
+
+	ret = snprintf(cgnamebuf, sizeof(cgnamebuf), "%s/%s/%s/cgroup.kill",
+		       CGROUP_BASEDIR, service, instance);
+	if (ret >= (int)sizeof(cgnamebuf))
+		return;
+
+	fd = open(cgnamebuf, O_WRONLY);
+	if (fd >= 0) {
+		if (write(fd, "1", 1) < 0)
+			ret = -1;
+		close(fd);
+	}
+
+	sep = strrchr(cgnamebuf, '/');
+	if (sep)
+		*sep = '\0';
+	(void)rmdir(cgnamebuf);
+
+	sep = strrchr(cgnamebuf, '/');
+	if (sep)
+		*sep = '\0';
+	(void)rmdir(cgnamebuf);
+}
+
+static void
 instance_free_stdio(struct service_instance *in)
 {
 	if (in->_stdout.fd.fd > -1) {
@@ -1601,6 +1631,7 @@ instance_free(struct service_instance *in)
 	uloop_timeout_cancel(&in->watchdog.timeout);
 	trigger_del(in);
 	watch_del(in);
+	instance_remove_cgroup(in->srv->name, in->name);
 	instance_config_cleanup(in);
 	free(in->config);
 	free(in->data_blob);

jail/cgroups.c
@@ -168,13 +168,39 @@ int cgroups_reclaim(int64_t bytes, int32_t swappiness)
 	return ret;
 }
 
-void cgroups_apply(pid_t pid)
+int cgroups_attach_pid(pid_t pid)
+{
+	char *ent;
+	int fd, ret = 0;
+	size_t len;
+
+	if (!cgroup_path)
+		return -ENODEV;
+
+	len = strlen(cgroup_path) + strlen("/cgroup.procs") + 1;
+	ent = malloc(len);
+	if (!ent)
+		return -ENOMEM;
+
+	snprintf(ent, len, "%s/cgroup.procs", cgroup_path);
+	fd = open(ent, O_WRONLY);
+	if (fd < 0) {
+		ret = -errno;
+		free(ent);
+		return ret;
+	}
+
+	if (dprintf(fd, "%d", pid) < 0)
+		ret = -errno;
+
+	close(fd);
+	free(ent);
+	return ret;
+}
+
+static void cgroups_compute_subtree_control(char *out, size_t outlen)
 {
 	struct cgval *valp;
-	char *cdir, *ent;
-	int fd;
-	size_t maxlen = strlen("cgroup.subtree_control");
-
 	bool cpuset = false,
 	     cpu = false,
 	     hugetlb = false,
@@ -182,17 +208,12 @@ void cgroups_apply(pid_t pid)
 	     memory = false,
 	     pids = false,
 	     rdma = false;
+	char *p;
 
-	char subtree_control[64] = { 0 };
+	out[0] = '\0';
 
-	DEBUG("using cgroup path %s\n", cgroup_path);
-	mkdir_p(cgroup_path, 0700);
-
-	/* find which controllers need to be enabled */
 	avl_for_each_element(&cgvals, valp, avl) {
-		ent = (char *)valp->avl.key;
-		if (strlen(ent) > maxlen)
-			maxlen = strlen(ent);
+		const char *ent = (const char *)valp->avl.key;
 
 		if (!strncmp("cpuset.", ent, 7))
 			cpuset = true;
@@ -210,36 +231,71 @@ void cgroups_apply(pid_t pid)
 			rdma = true;
 	}
 
-	maxlen += strlen(cgroup_path) + 2;
-
 	if (cpuset)
-		strcat(subtree_control, "+cpuset ");
+		strncat(out, "+cpuset ", outlen - strlen(out) - 1);
 
 	if (cpu)
-		strcat(subtree_control, "+cpu ");
+		strncat(out, "+cpu ", outlen - strlen(out) - 1);
 
 	if (hugetlb)
-		strcat(subtree_control, "+hugetlb ");
+		strncat(out, "+hugetlb ", outlen - strlen(out) - 1);
 
 	if (io)
-		strcat(subtree_control, "+io ");
+		strncat(out, "+io ", outlen - strlen(out) - 1);
 
 	if (memory)
-		strcat(subtree_control, "+memory ");
+		strncat(out, "+memory ", outlen - strlen(out) - 1);
 
 	if (pids)
-		strcat(subtree_control, "+pids ");
+		strncat(out, "+pids ", outlen - strlen(out) - 1);
 
 	if (rdma)
-		strcat(subtree_control, "+rdma ");
+		strncat(out, "+rdma ", outlen - strlen(out) - 1);
 
-	/* remove trailing space (length is > 0) */
-	ent = strchr(subtree_control, '\0');
-	if (ent > subtree_control) {
-		ent -= 1;
-		*ent = '\0';
+	p = strchr(out, '\0');
+	if (p > out && p[-1] == ' ')
+		p[-1] = '\0';
+}
+
+void cgroups_destroy(void)
+{
+	char *sep;
+
+	if (!cgroup_path)
+		return;
+
+	cgroups_kill_all();
+
+	(void)rmdir(cgroup_path);
+
+	sep = strrchr(cgroup_path, '/');
+	if (sep && sep != cgroup_path) {
+		*sep = '\0';
+		(void)rmdir(cgroup_path);
+		*sep = '/';
+	}
+}
+
+void cgroups_create(void)
+{
+	char subtree_control[64] = { 0 };
+	char *cdir, *ent;
+	size_t maxlen;
+	int fd;
+
+	if (!cgroup_path)
+		return;
+
+	DEBUG("creating cgroup %s\n", cgroup_path);
+	mkdir_p(cgroup_path, 0700);
+
+	cgroups_compute_subtree_control(subtree_control, sizeof(subtree_control));
+	if (!subtree_control[0]) {
+		DEBUG("no cgroup controllers requested, skipping subtree_control walk\n");
+		return;
 	}
 
+	maxlen = strlen(cgroup_path) + strlen("/cgroup.subtree_control") + 1;
 	ent = malloc(maxlen);
 	if (!ent)
 		exit(ENOMEM);
@@ -252,18 +308,38 @@ void cgroups_apply(pid_t pid)
 		DEBUG(" * %s\n", ent);
 		if ((fd = open(ent, O_WRONLY)) < 0) {
 			ERROR("can't open %s: %m\n", ent);
+			*cdir = '/';
 			continue;
 		}
-
-		if (write(fd, subtree_control, strlen(subtree_control)) == -1) {
+		if (write(fd, subtree_control, strlen(subtree_control)) == -1)
 			ERROR("can't write to %s: %m\n", ent);
-			close(fd);
-			continue;
-		}
-
 		close(fd);
 		*cdir = '/';
 	}
+	free(ent);
+}
+
+void cgroups_configure(void)
+{
+	struct cgval *valp;
+	char *ent;
+	size_t maxlen = 0;
+	int fd, dirfd;
+
+	if (!cgroup_path)
+		return;
+
+	avl_for_each_element(&cgvals, valp, avl) {
+		size_t klen = strlen((char *)valp->avl.key);
+
+		if (klen > maxlen)
+			maxlen = klen;
+	}
+	maxlen += strlen(cgroup_path) + 2;
+
+	ent = malloc(maxlen);
+	if (!ent)
+		exit(ENOMEM);
 
 	avl_for_each_element(&cgvals, valp, avl) {
 		DEBUG("applying cgroup2 %s=\"%s\"\n", (char *)valp->avl.key, valp->val);
@@ -273,30 +349,26 @@ void cgroups_apply(pid_t pid)
 			ERROR("can't open %s: %m\n", ent);
 			continue;
 		}
-		if (dprintf(fd, "%s", valp->val) < 0) {
+		if (dprintf(fd, "%s", valp->val) < 0)
 			ERROR("can't write to %s: %m\n", ent);
-		};
 		close(fd);
 	}
+	free(ent);
 
-	int dirfd = open(cgroup_path, O_DIRECTORY);
+	dirfd = open(cgroup_path, O_DIRECTORY);
 	if (dirfd < 0) {
 		ERROR("can't open %s: %m\n", cgroup_path);
 	} else {
 		attach_cgroups_ebpf(dirfd);
 		close(dirfd);
 	}
+}
 
-	snprintf(ent, maxlen, "%s/%s", cgroup_path, "cgroup.procs");
-	fd = open(ent, O_WRONLY);
-	if (fd < 0) {
-		ERROR("can't open %s: %m\n", cgroup_path);
-	} else {
-		dprintf(fd, "%d", pid);
-		close(fd);
-	}
-
-	free(ent);
+void cgroups_apply(pid_t pid)
+{
+	cgroups_create();
+	cgroups_configure();
+	cgroups_attach_pid(pid);
 }
 
 enum {
@@ -806,6 +878,13 @@ int cgroups_open_attr(const char *attr)
 
 	snprintf(path, sizeof(path), "%s/%s", cgroup_path, attr);
 	return open(path, O_RDONLY | O_CLOEXEC);
+}
+
+int cgroups_open_dir(void)
+{
+	if (!cgroup_path)
+		return -1;
+	return open(cgroup_path, O_PATH | O_DIRECTORY | O_CLOEXEC);
 }
 
 void cgroups_set_memory_limit(int64_t bytes)

jail/cgroups.h
@@ -19,12 +19,17 @@
 
 void cgroups_init(const char *p);
 int parseOCIlinuxcgroups(struct blob_attr *msg);
+void cgroups_create(void);
+void cgroups_destroy(void);
+void cgroups_configure(void);
 void cgroups_apply(pid_t pid);
+int cgroups_attach_pid(pid_t pid);
 int cgroups_kill_all(void);
 int cgroups_set_frozen(bool frozen);
 int cgroups_reclaim(int64_t bytes, int32_t swappiness);
 int64_t cgroups_read_int64(const char *attr);
 int cgroups_open_attr(const char *attr);
+int cgroups_open_dir(void);
 void cgroups_free(void);
 void cgroups_prepare(void);
 void cgroups_set_memory_limit(int64_t bytes);

jail/jail.c
@@ -50,6 +50,7 @@
 #include <linux/filter.h>
 #include <linux/limits.h>
 #include <linux/nsfs.h>
+#include <linux/sched.h>
 #include <linux/securebits.h>
 #include <signal.h>
 #include <inttypes.h>
@@ -79,7 +80,6 @@
 #define CLONE_NEWTIME 0x00000080
 #endif
 
-#define STACK_SIZE	(1024 * 1024)
 #define OPT_ARGS	"cC:d:De:EfFG:h:ij:J:ln:NoO:pP:r:R:sS:uU:w:t:T:y"
 
 #define OCI_VERSION_STRING "1.0.2"
@@ -191,7 +191,12 @@ extern int pivot_root(const char *new_root, const char *put_old);
 
 int debug = 0;
 
-static char child_stack[STACK_SIZE];
+static long jail_clone3(struct clone_args *args)
+{
+	return syscall(SYS_clone3, args, sizeof(*args));
+}
+
+static int jail_process_pidfd = -1;
 
 static struct ubus_context *parent_ctx;
 
@@ -1159,8 +1164,10 @@ static int build_jail_fs(void)
 static bool exit_from_child;
 static void free_and_exit(int ret)
 {
-	if (!exit_from_child && opts.ocibundle)
+	if (!exit_from_child && opts.ocibundle) {
+		cgroups_destroy();
 		cgroups_free();
+	}
 
 	if (!exit_from_child && parent_ctx)
 		ubus_free(parent_ctx);
@@ -1837,10 +1844,17 @@ static struct uloop_process jail_process = {
 	.cb = jail_process_handler,
 };
 
+static int jail_pidfd_send_signal(int sig)
+{
+	if (jail_process_pidfd < 0)
+		return kill(jail_process.pid, sig);
+	return syscall(SYS_pidfd_send_signal, jail_process_pidfd, sig, NULL, 0);
+}
+
 static void jail_process_timeout_cb(struct uloop_timeout *t)
 {
 	DEBUG("jail process failed to stop, sending SIGKILL\n");
-	kill(jail_process.pid, SIGKILL);
+	jail_pidfd_send_signal(SIGKILL);
 }
 
 static void jail_handle_signal(int signo)
@@ -1855,7 +1869,7 @@ static void jail_handle_signal(int signo)
 
 	if (jail_running) {
 		DEBUG("forwarding signal %d to the jailed process\n", signo);
-		kill(jail_process.pid, signo);
+		jail_pidfd_send_signal(signo);
 		/* set timeout to send SIGKILL jail process in case SIGTERM doesn't succeed */
 		if (signo == SIGTERM)
 			uloop_timeout_set(&jail_process_timeout, opts.term_timeout * 1000);
@@ -4129,13 +4143,14 @@ container_handle_kill(struct ubus_context *ctx, struct ubus_object *obj,
 		DEBUG("cgroup.kill unavailable (%d), falling back to per-pid kill\n", rc);
 	}
 
-	if (kill(jail_process.pid, sig) == 0)
+	if (jail_pidfd_send_signal(sig) == 0)
 		return 0;
 
 	switch (errno) {
 	case EINVAL: return UBUS_STATUS_INVALID_ARGUMENT;
 	case EPERM:  return UBUS_STATUS_PERMISSION_DENIED;
 	case ESRCH:  return UBUS_STATUS_NOT_FOUND;
+	case EBADF:  return UBUS_STATUS_UNKNOWN_ERROR;
 	}
 
 	return UBUS_STATUS_UNKNOWN_ERROR;
@@ -4707,6 +4722,9 @@ static void post_main(struct uloop_timeout *t)
 
 	parent_pidfd = syscall(SYS_pidfd_open, getpid(), 0);
 
+	if (opts.ocibundle)
+		cgroups_create();
+
 	if (has_namespaces()) {
 		if (opts.namespace & CLONE_NEWNS) {
 			if (!opts.extroot && (opts.user || opts.group)) {
@@ -4852,7 +4870,24 @@ static void post_main(struct uloop_timeout *t)
 		 * CLONE_NEWUSER is excluded here; the child creates its own
 		 * later, in enter_userns(). See exec_jail() for why.
 		 */
-		jail_process.pid = clone(exec_jail, child_stack + STACK_SIZE, SIGCHLD | (opts.namespace & ~(CLONE_NEWCGROUP | CLONE_NEWUSER | CLONE_NEWTIME)), NULL);
+		int init_cgroup_fd = -1;
+		struct clone_args cargs = {
+			.flags = (opts.namespace & ~(CLONE_NEWCGROUP | CLONE_NEWUSER | CLONE_NEWTIME)) | CLONE_PIDFD,
+			.pidfd = (__u64)(uintptr_t)&jail_process_pidfd,
+			.exit_signal = SIGCHLD,
+		};
+
+		if (opts.ocibundle) {
+			init_cgroup_fd = cgroups_open_dir();
+			if (init_cgroup_fd >= 0) {
+				cargs.flags |= CLONE_INTO_CGROUP;
+				cargs.cgroup = (__u64)init_cgroup_fd;
+			}
+		}
+
+		jail_process.pid = jail_clone3(&cargs);
+		if (init_cgroup_fd >= 0)
+			close(init_cgroup_fd);
 	} else {
 		jail_process.pid = fork();
 	}
@@ -4902,8 +4937,10 @@ static void post_main(struct uloop_timeout *t)
 		close(pipes[0]);
 		set_oom_score_adj();
 
-		if (opts.ocibundle)
-			cgroups_apply(jail_process.pid);
+		if (opts.ocibundle) {
+			cgroups_configure();
+			cgroups_attach_pid(jail_process.pid);
+		}
 
 		if (opts.namespace & CLONE_NEWNET)
 			jail_network_start(parent_ctx, opts.name, jail_process.pid);
@@ -5065,7 +5102,7 @@ static void post_poststart(void)
 
 	if (jail_running) {
 		DEBUG("killing jail process\n");
-		kill(jail_process.pid, SIGTERM);
+		jail_pidfd_send_signal(SIGTERM);
 		uloop_timeout_set(&jail_process_timeout, 1000);
 		uloop_run();
 	}
@@ -5085,6 +5122,10 @@ static void poststop(void) {
 
 static void post_poststop(void)
 {
+	if (jail_process_pidfd >= 0) {
+		close(jail_process_pidfd);
+		jail_process_pidfd = -1;
+	}
 	free_opts(true);
 	if (parent_ctx)
 		ubus_free(parent_ctx);

commit 3f7284bfedab20e421fc7f74c42dbbca2bef4df5

instance: key cgroup teardown on ownership, not on the name
instance_free() reclaims /sys/fs/cgroup/services/<service>/<instance>
from the service and instance names alone, and the cgroup.kill it
writes takes down every process in that cgroup and its descendants. A
service set naming an instance that already exists reaches
instance_free() through service_instance_update(), which frees the new
object after instance_update() has merged it into the running one. That
object is never inserted into the instance vlist, since no_delete makes
libubox keep the old node, yet it carries the same two names, so
freeing it kills and removes the cgroup of the live instance: the
daemon and everything it forked die, and procd, which intended no stop,
sees a crash and respawns after the backoff.

Record in instance_start() that a fork has claimed the cgroup path and
reclaim only what was claimed. trigger_del() and watch_del() beside it
key on the instance pointer and were never affected; only the cgroup
teardown identified its target by name.

Fixes: f4d512d93a00 ("jail: place container init and exec into the target cgroup via clone3")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

service/instance.c
@@ -795,6 +795,7 @@ instance_start(struct service_instance *in)
 
 	P_DEBUG(2, "Started instance %s::%s[%d]\n", in->srv->name, in->name, pid);
 	in->proc.pid = pid;
+	in->has_cgroup = true;
 	instance_writepid(in);
 	clock_gettime(CLOCK_MONOTONIC, &in->start);
 	uloop_process_add(&in->proc);
@@ -1816,7 +1817,9 @@ instance_free(struct service_instance *in)
 	uloop_timeout_cancel(&in->watchdog.timeout);
 	trigger_del(in);
 	watch_del(in);
-	instance_remove_cgroup(in->srv->name, in->name);
+	if (in->has_cgroup)
+		instance_remove_cgroup(in->srv->name, in->name);
+
 	instance_config_cleanup(in);
 	free(in->config);
 	free(in->data_blob);

service/instance.h
@@ -79,6 +79,7 @@ struct service_instance {
 	char *group;
 	gid_t gr_gid;
 
+	bool has_cgroup;
 	bool halt;
 	bool restart;
 	bool respawn;