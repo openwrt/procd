openwrt/procd

commit f1bf99b09f664dce09f55e7102ce8e6ef77ef10a

add signal handler

signal.c
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Felix Fietkau <nbd@openwrt.org>
+ * Copyright (C) 2013 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <sys/reboot.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include "procd.h"
+
+static void signal_shutdown(int signal, siginfo_t *siginfo, void *data)
+{
+	int event = 0;
+	char *msg = NULL;
+
+	switch(signal) {
+	case SIGTERM:
+		event = RB_AUTOBOOT;
+		msg = "reboot";
+		break;
+	case SIGUSR1:
+	case SIGUSR2:
+		event = RB_POWER_OFF;
+		msg = "poweroff";
+		break;
+	}
+
+	DEBUG(1, "Triggering %s\n", msg);
+	if (event)
+		procd_shutdown(event);
+}
+
+struct sigaction sa_shutdown = {
+	.sa_sigaction = signal_shutdown,
+	.sa_flags = SA_SIGINFO
+};
+
+static void signal_crash(int signal, siginfo_t *siginfo, void *data)
+{
+	ERROR("Rebooting as procd has crashed\n");
+	fflush(stderr);
+	sync();
+	sleep(1);
+	reboot(RB_AUTOBOOT);
+	while (1)
+		;
+}
+
+struct sigaction sa_crash = {
+	.sa_sigaction = signal_crash,
+	.sa_flags = SA_SIGINFO
+};
+
+static void signal_dummy(int signal, siginfo_t *siginfo, void *data)
+{
+	ERROR("Got unexpected signal %d\n", signal);
+}
+
+struct sigaction sa_dummy = {
+	.sa_sigaction = signal_dummy,
+	.sa_flags = SA_SIGINFO
+};
+
+void procd_signal(void)
+{
+	sigaction(SIGPIPE, &sa_dummy, NULL);
+	if (getpid() != 1)
+		return;
+	sigaction(SIGTERM, &sa_shutdown, NULL);
+	sigaction(SIGUSR1, &sa_shutdown, NULL);
+	sigaction(SIGUSR2, &sa_shutdown, NULL);
+	sigaction(SIGSEGV, &sa_crash, NULL);
+	sigaction(SIGBUS, &sa_crash, NULL);
+	sigaction(SIGHUP, &sa_dummy, NULL);
+	sigaction(SIGKILL, &sa_dummy, NULL);
+	sigaction(SIGSTOP, &sa_dummy, NULL);
+}

commit 46dc5e51faed205bda5514e03a70136a50add9f9

signal: drop the unsafe stdio calls from do_reboot()
do_reboot() has exactly one caller, signal_crash(), so every statement
in it runs inside a SIGSEGV or SIGBUS handler. Neither fflush() nor
ulog() is async-signal-safe: both take a lock, so a crash raised inside
any stdio or syslog call deadlocks procd against itself instead of
rebooting the box. The flush has had nothing to flush since procd's own
messages stopped travelling on stderr, and the line it announced is now
written to /dev/kmsg by the caller. Only sync(), sleep() and reboot()
remain.

Fixes: f1bf99b09f66 ("add signal handler")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

signal.c
@@ -47,8 +47,6 @@ static void crash_kmsg(void)
 
 static void do_reboot(void)
 {
-	LOG("reboot\n");
-	fflush(stderr);
 	sync();
 	sleep(2);
 	reboot(RB_AUTOBOOT);