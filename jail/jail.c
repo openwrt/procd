openwrt/procd

commit fc9f614bf701d1531cc2969b6c66a7f4d12d2118

jail: parse and run OCI hooks
OCI run-time allows defining hooks to be executed at various stages of
the container life cycle. Parse hooks and run them accordingly.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

jail/jail.c
@@ -53,6 +53,13 @@
 #define STACK_SIZE	(1024 * 1024)
 #define OPT_ARGS	"S:C:n:h:r:w:d:psulocU:G:NR:fFO:T:EyJ:"
 
+struct hook_execvpe {
+	char *file;
+	char **argv;
+	char **envp;
+	int timeout;
+};
+
 static struct {
 	char *name;
 	char *hostname;
@@ -80,6 +87,13 @@ static struct {
 	int pw_gid;
 	int gr_gid;
 	int require_jail;
+	struct {
+		struct hook_execvpe *createRuntime;
+		struct hook_execvpe *createContainer;
+		struct hook_execvpe *startContainer;
+		struct hook_execvpe *poststart;
+		struct hook_execvpe *poststop;
+	} hooks;
 } opts;
 
 static struct blob_buf ocibuf;
@@ -301,6 +315,106 @@ no_console:
 	return 1;
 }
 
+static int hook_running = 0;
+static int hook_return_code = 0;
+
+static void hook_process_timeout_cb(struct uloop_timeout *t);
+static struct uloop_timeout hook_process_timeout = {
+	.cb = hook_process_timeout_cb,
+};
+
+static void hook_process_handler(struct uloop_process *c, int ret)
+{
+	uloop_timeout_cancel(&hook_process_timeout);
+	if (WIFEXITED(ret)) {
+		hook_return_code = WEXITSTATUS(ret);
+		INFO("hook (%d) exited with exit: %d\n", c->pid, hook_return_code);
+	} else {
+		hook_return_code = WTERMSIG(ret);
+		INFO("hook (%d) exited with signal: %d\n", c->pid, hook_return_code);
+	}
+	hook_running = 0;
+	uloop_end();
+}
+
+static struct uloop_process hook_process = {
+	.cb = hook_process_handler,
+};
+
+static void hook_process_timeout_cb(struct uloop_timeout *t)
+{
+	DEBUG("hook process failed to stop, sending SIGKILL\n");
+	kill(hook_process.pid, SIGKILL);
+}
+
+static void hook_handle_signal(int signo)
+{
+	DEBUG("forwarding signal %d to the jailed process\n", signo);
+	kill(hook_process.pid, signo);
+}
+
+static int run_hook(struct hook_execvpe *hook)
+{
+	sigset_t sigmask;
+	int i;
+
+	DEBUG("executing hook %s\n", hook->file);
+	uloop_init();
+	sigfillset(&sigmask);
+	for (i = 0; i < _NSIG; i++) {
+		struct sigaction s = { 0 };
+
+		if (!sigismember(&sigmask, i))
+			continue;
+		if ((i == SIGCHLD) || (i == SIGPIPE) || (i == SIGSEGV))
+			continue;
+
+		s.sa_handler = hook_handle_signal;
+		sigaction(i, &s, NULL);
+	}
+	hook_running = 1;
+	hook_process.pid = fork();
+	if (hook_process.pid > 0) {
+		/* parent */
+		uloop_process_add(&hook_process);
+		if (hook->timeout > 0)
+			uloop_timeout_set(&hook_process_timeout, 1000 * hook->timeout);
+
+		uloop_run();
+		if (hook_running) {
+			DEBUG("uloop interrupted, killing hook process\n");
+			kill(hook_process.pid, SIGTERM);
+			uloop_timeout_set(&hook_process_timeout, 1000);
+			uloop_run();
+		}
+		uloop_done();
+
+		return 0;
+	} else if (hook_process.pid == 0) {
+		/* child */
+		execvpe(hook->file, hook->argv, hook->envp);
+		return errno;
+	} else {
+		/* fork error */
+		return errno;
+	}
+}
+
+static int run_hooks(struct hook_execvpe *hook)
+{
+	struct hook_execvpe *cur;
+	int res;
+
+	cur = hook;
+	while (cur) {
+		res = run_hook(cur++);
+		if (res)
+			return res;
+	}
+
+	return 0;
+}
+
 static int build_jail_fs(void)
 {
 	char jail_root[] = "/tmp/ujail-XXXXXX";
@@ -391,6 +505,8 @@ static int build_jail_fs(void)
 			unlink(jaillink);
 		symlink("../tmp/resolv.conf.d/resolv.conf.auto", jaillink);
 	}
+
+	run_hooks(opts.hooks.createContainer);
 
 	char dirbuf[sizeof(jail_root) + 4];
 	snprintf(dirbuf, sizeof(dirbuf), "%s/old", jail_root);
@@ -689,6 +805,8 @@ static int exec_jail(void *pipes_ptr)
 		exit(EXIT_FAILURE);
 	}
 
+	run_hooks(opts.hooks.startContainer);
+
 	if (applyOCIcapabilities(opts.capset))
 		exit(EXIT_FAILURE);
 
@@ -796,6 +914,32 @@ static void netns_updown(pid_t pid, bool start)
 	ubus_free(ctx);
 }
 
+static int parseOCIenvarray(struct blob_attr *msg, char ***envp)
+{
+	struct blob_attr *cur;
+	int sz = 0, rem;
+
+	blobmsg_for_each_attr(cur, msg, rem)
+		++sz;
+
+	if (sz > 0) {
+		*envp = calloc(1 + sz, sizeof(char*));
+		if (!(*envp))
+			return ENOMEM;
+	} else {
+		*envp = NULL;
+		return 0;
+	}
+
+	sz = 0;
+	blobmsg_for_each_attr(cur, msg, rem)
+		(*envp)[sz++] = strdup(blobmsg_get_string(cur));
+
+	if (sz)
+		(*envp)[sz] = NULL;
+
+	return 0;
+}
 
 enum {
 	OCI_ROOT_PATH,
@@ -835,6 +979,170 @@ static int parseOCIroot(const char *jsonfile, struct blob_attr *msg)
 	return 0;
 }
 
+
+enum {
+	OCI_HOOK_PATH,
+	OCI_HOOK_ARGS,
+	OCI_HOOK_ENV,
+	OCI_HOOK_TIMEOUT,
+	__OCI_HOOK_MAX,
+};
+
+static const struct blobmsg_policy oci_hook_policy[] = {
+	[OCI_HOOK_PATH] = { "path", BLOBMSG_TYPE_STRING },
+	[OCI_HOOK_ARGS] = { "args", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOK_ENV] = { "env", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOK_TIMEOUT] = { "timeout", BLOBMSG_TYPE_INT32 },
+};
+
+static void free_hooklist(struct hook_execvpe *hooklist)
+{
+	struct hook_execvpe *cur;
+	char **tmp;
+
+	cur = hooklist;
+	while (cur) {
+		tmp = cur->argv;
+		while (tmp)
+			free(tmp++);
+
+		tmp = cur->envp;
+		while (tmp)
+			free(tmp++);
+
+		free(cur->file);
+		free(cur++);
+	}
+}
+
+static int parseOCIhook(struct hook_execvpe **hooklist, struct blob_attr *msg)
+{
+	struct blob_attr *tb[__OCI_HOOK_MAX];
+	struct blob_attr *cur;
+	int rem, ret = 0;
+	int idx = 0;
+
+	blobmsg_for_each_attr(cur, msg, rem)
+		++idx;
+
+	if (!idx)
+		goto errout;
+
+	*hooklist = calloc(idx + 1, sizeof(struct hook_execvpe *));
+	idx = 0;
+
+	blobmsg_for_each_attr(cur, msg, rem) {
+		blobmsg_parse(oci_hook_policy, __OCI_HOOK_MAX, tb, blobmsg_data(cur), blobmsg_len(cur));
+
+		if (!tb[OCI_HOOK_PATH]) {
+			ret = EINVAL;
+			goto errout;
+		}
+
+		hooklist[idx] = malloc(sizeof(struct hook_execvpe));
+		if (tb[OCI_HOOK_ARGS]) {
+			ret = parseOCIenvarray(tb[OCI_HOOK_ARGS], &(hooklist[idx]->argv));
+			if (ret)
+				goto errout;
+		}
+
+		if (tb[OCI_HOOK_ENV]) {
+			ret = parseOCIenvarray(tb[OCI_HOOK_ENV], &(hooklist[idx]->envp));
+			if (ret)
+				goto errout;
+		}
+
+		if (tb[OCI_HOOK_TIMEOUT])
+			hooklist[idx]->timeout = blobmsg_get_u32(tb[OCI_HOOK_TIMEOUT]);
+
+		hooklist[idx]->file = strdup(blobmsg_get_string(tb[OCI_HOOK_PATH]));
+
+		++idx;
+	}
+
+	hooklist[idx] = NULL;
+	return 0;
+
+errout:
+	free_hooklist(*hooklist);
+	*hooklist = NULL;
+
+	return ret;
+};
+
+
+enum {
+	OCI_HOOKS_PRESTART,
+	OCI_HOOKS_CREATERUNTIME,
+	OCI_HOOKS_CREATECONTAINER,
+	OCI_HOOKS_STARTCONTAINER,
+	OCI_HOOKS_POSTSTART,
+	OCI_HOOKS_POSTSTOP,
+	__OCI_HOOKS_MAX,
+};
+
+static const struct blobmsg_policy oci_hooks_policy[] = {
+	[OCI_HOOKS_PRESTART] = { "prestart", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS_CREATERUNTIME] = { "createRuntime", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS_CREATECONTAINER] = { "createContainer", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS_STARTCONTAINER] = { "startContainer", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS_POSTSTART] = { "poststart", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS_POSTSTOP] = { "poststop", BLOBMSG_TYPE_ARRAY },
+};
+
+static int parseOCIhooks(struct blob_attr *msg)
+{
+	struct blob_attr *tb[__OCI_HOOKS_MAX];
+	int ret;
+
+	blobmsg_parse(oci_hooks_policy, __OCI_HOOKS_MAX, tb, blobmsg_data(msg), blobmsg_len(msg));
+
+	if (tb[OCI_HOOKS_PRESTART])
+		INFO("warning: ignoring deprecated prestart hook");
+
+	if (tb[OCI_HOOKS_CREATERUNTIME]) {
+		ret = parseOCIhook(&opts.hooks.createRuntime, tb[OCI_HOOKS_CREATERUNTIME]);
+		if (ret)
+			return ret;
+	}
+
+	if (tb[OCI_HOOKS_CREATECONTAINER]) {
+		ret = parseOCIhook(&opts.hooks.createContainer, tb[OCI_HOOKS_CREATECONTAINER]);
+		if (ret)
+			goto out_createruntime;
+	}
+
+	if (tb[OCI_HOOKS_STARTCONTAINER]) {
+		ret = parseOCIhook(&opts.hooks.startContainer, tb[OCI_HOOKS_STARTCONTAINER]);
+		if (ret)
+			goto out_createcontainer;
+	}
+
+	if (tb[OCI_HOOKS_POSTSTART]) {
+		ret = parseOCIhook(&opts.hooks.poststart, tb[OCI_HOOKS_POSTSTART]);
+		if (ret)
+			goto out_startcontainer;
+	}
+
+	if (tb[OCI_HOOKS_POSTSTOP]) {
+		ret = parseOCIhook(&opts.hooks.poststop, tb[OCI_HOOKS_POSTSTOP]);
+		if (ret)
+			goto out_poststart;
+	}
+
+	return 0;
+
+out_poststart:
+	free_hooklist(opts.hooks.poststart);
+out_startcontainer:
+	free_hooklist(opts.hooks.startContainer);
+out_createcontainer:
+	free_hooklist(opts.hooks.createContainer);
+out_createruntime:
+	free_hooklist(opts.hooks.createRuntime);
+
+	return ret;
+};
 
 enum {
 	OCI_MOUNT_SOURCE,
@@ -944,12 +1252,10 @@ static const struct blobmsg_policy oci_process_policy[] = {
 	[OCI_PROCESS_USER] = { "user", BLOBMSG_TYPE_TABLE },
 };
 
+
 static int parseOCIprocess(struct blob_attr *msg)
 {
 	struct blob_attr *tb[__OCI_PROCESS_MAX];
-	struct blob_attr *cur;
-	unsigned int sz = 0;
-	int rem;
 	int res;
 
 	blobmsg_parse(oci_process_policy, __OCI_PROCESS_MAX, tb, blobmsg_data(msg), blobmsg_len(msg));
@@ -957,19 +1263,9 @@ static int parseOCIprocess(struct blob_attr *msg)
 	if (!tb[OCI_PROCESS_ARGS])
 		return ENOENT;
 
-	blobmsg_for_each_attr(cur, tb[OCI_PROCESS_ARGS], rem)
-		++sz;
-
-	if (!sz)
-		return ENODATA;
-
-	opts.jail_argv = calloc(1 + sz, sizeof(char*));
-	if (!opts.jail_argv)
-		return ENOMEM;
-
-	sz = 0;
-	blobmsg_for_each_attr(cur, tb[OCI_PROCESS_ARGS], rem)
-		opts.jail_argv[sz++] = blobmsg_get_string(cur);
+	res = parseOCIenvarray(tb[OCI_PROCESS_ARGS], &opts.jail_argv);
+	if (res)
+		return res;
 
 	opts.console = blobmsg_get_bool(tb[OCI_PROCESS_TERMINAL]);
 	opts.no_new_privs = blobmsg_get_bool(tb[OCI_PROCESS_NONEWPRIVILEGES]);
@@ -977,19 +1273,11 @@ static int parseOCIprocess(struct blob_attr *msg)
 	if (tb[OCI_PROCESS_CWD])
 		opts.cwd = blobmsg_get_string(tb[OCI_PROCESS_CWD]);
 
-	sz = 0;
-	blobmsg_for_each_attr(cur, tb[OCI_PROCESS_ENV], rem)
-		++sz;
-
-	if (sz > 0) {
-		opts.envp = calloc(1 + sz, sizeof(char*));
-		if (!opts.envp)
-			return ENOMEM;
+	if (tb[OCI_PROCESS_ENV]) {
+		res = parseOCIenvarray(tb[OCI_PROCESS_ENV], &opts.envp);
+		if (res)
+			return res;
 	}
-
-	sz = 0;
-	blobmsg_for_each_attr(cur, tb[OCI_PROCESS_ENV], rem)
-		opts.envp[sz++] = strdup(blobmsg_get_string(cur));
 
 	if (tb[OCI_PROCESS_USER] && (res = parseOCIprocessuser(tb[OCI_PROCESS_USER])))
 		return res;
@@ -1203,6 +1491,7 @@ enum {
 	OCI_PROCESS,
 	OCI_ROOT,
 	OCI_MOUNTS,
+	OCI_HOOKS,
 	OCI_LINUX,
 	__OCI_MAX,
 };
@@ -1213,6 +1502,7 @@ static const struct blobmsg_policy oci_policy[] = {
 	[OCI_PROCESS] = { "process", BLOBMSG_TYPE_TABLE },
 	[OCI_ROOT] = { "root", BLOBMSG_TYPE_TABLE },
 	[OCI_MOUNTS] = { "mounts", BLOBMSG_TYPE_ARRAY },
+	[OCI_HOOKS] = { "hooks", BLOBMSG_TYPE_TABLE },
 	[OCI_LINUX] = { "linux", BLOBMSG_TYPE_TABLE },
 };
 
@@ -1260,6 +1550,9 @@ static int parseOCI(const char *jsonfile)
 			return res;
 
 	if (tb[OCI_LINUX] && (res = parseOCIlinux(tb[OCI_LINUX])))
+		return res;
+
+	if (tb[OCI_HOOKS] && (res = parseOCIhooks(tb[OCI_HOOKS])))
 		return res;
 
 	return 0;
@@ -1481,6 +1774,7 @@ int main(int argc, char **argv)
 		/* parent process */
 		close(pipes[1]);
 		close(pipes[2]);
+		run_hooks(opts.hooks.createRuntime);
 		if (read(pipes[0], sig_buf, 1) < 1) {
 			ERROR("can't read from child\n");
 			return -1;
@@ -1522,6 +1816,7 @@ int main(int argc, char **argv)
 			return -1;
 		}
 		close(pipes[3]);
+		run_hooks(opts.hooks.poststart);
 		uloop_process_add(&jail_process);
 		uloop_run();
 		if (jail_running) {
@@ -1536,6 +1831,7 @@ int main(int argc, char **argv)
 			netns_updown(getpid(), false);
 			close(netns_fd);
 		}
+		run_hooks(opts.hooks.poststop);
 		return jail_return_code;
 	} else if (jail_process.pid == 0) {
 		/* fork child process */

commit 869017287c044880fbec6a916b07b7bc0e9eaaef

jail, uxc: run the poststop hooks at delete time
runtime-spec v1.3.0 runtime.md#lifecycle puts the poststop hooks at
step 13, after the container is destroyed and before delete returns.
ujail runs them when the container process is reaped and exits at once,
so they land between steps 10 and 11, before any delete.

Park a registered container in the stopped state and drive the hooks
from a delete instead, either the new method on container.<name>, whose
reply is deferred until the chain has finished, or the SIGTERM procd
sends an instance it is tearing down. A delete asked for before the
container has stopped is honoured rather than parked, which is what a
forced delete and every procd-driven stop look like; otherwise procd
would SIGKILL the parked supervisor and the hooks would be skipped.

uxc waits for the deferred reply or for ubus.object.remove of
container.<name>, whichever comes first: ubusd_proto_free_client()
frees a dead client's objects without answering anyone blocked on
them, so a ujail that died mid-invoke would never release the caller.

A seccomp handshake that failed on an already reaped payload stops
exiting on its own, and uxc drops a stopped incarnation before creating
over it, instance_update() having nothing to do for a pending process
and an unchanged config. A netifd restart still runs the hooks at
payload exit, re-execing in place with no delete behind it.

Fixes: fc9f614bf701 ("jail: parse and run OCI hooks")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

jail/jail.c
@@ -262,6 +262,10 @@ static volatile sig_atomic_t jail_stop_requested;
 static bool netifd_restart_pending;
 static bool container_registered;
 static bool stop_announced;
+static volatile sig_atomic_t delete_requested;
+static bool delete_awaited;
+static bool delete_deferred;
+static struct ubus_request_data delete_request;
 static char **restart_argv;
 
 static const char *jail_reason;
@@ -1762,6 +1766,9 @@ static void free_and_exit(int ret)
 							  "instance.create_failed");
 	}
 
+	if (!exit_from_child && parent_ctx && delete_deferred)
+		ubus_complete_deferred_request(parent_ctx, &delete_request, 0);
+
 	if (!exit_from_child && parent_ctx)
 		ubus_free(parent_ctx);
 
@@ -2524,10 +2531,25 @@ static void jail_process_timeout_cb(struct uloop_timeout *t)
 	jail_pidfd_send_signal(SIGKILL);
 }
 
+static void container_delete_cb(struct uloop_timeout *t)
+{
+	if (!delete_awaited)
+		return;
+
+	delete_awaited = false;
+	poststop_hooks();
+}
+
+static struct uloop_timeout container_delete_timeout = {
+	.cb = container_delete_cb,
+};
+
 static void jail_handle_signal(int signo)
 {
-	if (signo == SIGTERM)
+	if (signo == SIGTERM) {
 		jail_stop_requested = true;
+		delete_requested = true;
+	}
 
 	if (hook_running) {
 		DEBUG("forwarding signal %d to the hook process\n", signo);
@@ -2544,6 +2566,9 @@ static void jail_handle_signal(int signo)
 		if (signo == SIGTERM)
 			uloop_timeout_set(&jail_process_timeout, opts.term_timeout * 1000);
 	}
+
+	if (signo == SIGTERM && delete_awaited)
+		uloop_timeout_add(&container_delete_timeout);
 }
 
 static void signals_init(void)
@@ -6436,10 +6461,29 @@ static void add_volume(const char *source, const char *dest)
 	free(real);
 }
 
+static int
+container_handle_delete(struct ubus_context *ctx, struct ubus_object *obj,
+			struct ubus_request_data *req, const char *method,
+			struct blob_attr *msg)
+{
+	if (delete_deferred || (!delete_awaited && !jail_stop_requested))
+		return UBUS_STATUS_INVALID_ARGUMENT;
+
+	ubus_defer_request(ctx, req, &delete_request);
+	delete_deferred = true;
+	delete_requested = true;
+
+	if (delete_awaited)
+		uloop_timeout_add(&container_delete_timeout);
+
+	return UBUS_STATUS_OK;
+}
+
 static struct ubus_method container_methods[] = {
 	UBUS_METHOD_NOARG("start", handle_start),
 	UBUS_METHOD_NOARG("state", handle_state),
 	UBUS_METHOD("kill", container_handle_kill, container_kill_attrs),
+	UBUS_METHOD_NOARG("delete", container_handle_delete),
 	UBUS_METHOD_NOARG("pause", container_handle_pause),
 	UBUS_METHOD_NOARG("resume", container_handle_resume),
 	UBUS_METHOD("reclaim", container_handle_reclaim, container_reclaim_attrs),
@@ -7866,8 +7910,12 @@ static void pipe_send_start_container(struct uloop_timeout *t)
 	}
 	close(pipes[3]);
 
-	if (jail_seccomp_handshake())
-		free_and_exit(-1);
+	if (jail_seccomp_handshake()) {
+		if (!jail_reaped)
+			free_and_exit(-1);
+
+		return;
+	}
 
 	poststart_pending = true;
 }
@@ -7914,6 +7962,13 @@ static void poststop(void)
 	stop_announced = true;
 	emit_instance_event(container_registered ? "instance.stopped" :
 						  "instance.create_failed");
+
+	/* a restart re-execs in place, so no delete will ever follow it */
+	if (opts.ocibundle && container_registered && !jail_restarting() &&
+	    !delete_requested) {
+		delete_awaited = true;
+		return;
+	}
 
 	poststop_hooks();
 }

uxc.c
@@ -380,13 +380,20 @@ static struct ubus_event_handler uxc_wait_ev;
 static bool uxc_wait_armed;
 static bool uxc_uloop_ready;
 
+static void uxc_uloop_init(void)
+{
+	if (uxc_uloop_ready)
+		return;
+
+	uloop_init();
+	ubus_add_uloop(ctx);
+	uxc_uloop_ready = true;
+}
+
 static int uxc_wait_arm(struct uxc_wait_state *w)
 {
-	if (!uxc_uloop_ready) {
-		uloop_init();
-		ubus_add_uloop(ctx);
-		uxc_uloop_ready = true;
-	}
+	uxc_uloop_init();
+
 	if (!uxc_wait_armed) {
 		uxc_wait_ev.cb = uxc_wait_event_cb;
 		if (ubus_register_event_handler(ctx, &uxc_wait_ev, "instance.*"))
@@ -1525,10 +1532,85 @@ static void materialise_initenv(const char *name, struct blob_attr *initenv)
 	}
 }
 
+static const char *uxc_delete_path;
+static bool uxc_delete_replied;
+
+static void uxc_delete_done_cb(struct ubus_request *req, int ret)
+{
+	uxc_delete_replied = true;
+	uloop_end();
+}
+
+static void uxc_object_remove_cb(struct ubus_context *uctx,
+				 struct ubus_event_handler *ev,
+				 const char *type, struct blob_attr *msg)
+{
+	static const struct blobmsg_policy pol = {
+		.name = "path", .type = BLOBMSG_TYPE_STRING
+	};
+	struct blob_attr *tb;
+
+	if (!msg || !uxc_delete_path)
+		return;
+
+	blobmsg_parse(&pol, 1, &tb, blob_data(msg), blob_len(msg));
+	if (!tb || strcmp(blobmsg_get_string(tb), uxc_delete_path))
+		return;
+
+	uloop_end();
+}
+
+static struct ubus_event_handler uxc_object_remove_ev = {
+	.cb = uxc_object_remove_cb,
+};
+
+static void uxc_runtime_delete(const char *name)
+{
+	struct ubus_request req;
+	char *objname;
+	uint32_t id;
+
+	if (asprintf(&objname, "container.%s", name) == -1)
+		return;
+
+	if (ubus_lookup_id(ctx, objname, &id))
+		goto out;
+
+	uxc_uloop_init();
+
+	if (ubus_register_event_handler(ctx, &uxc_object_remove_ev,
+					"ubus.object.remove")) {
+		fprintf(stderr, "uxc: cannot watch for %s to be removed\n",
+			objname);
+		goto out;
+	}
+
+	if (ubus_invoke_async(ctx, id, "delete", NULL, &req))
+		goto unregister;
+
+	uxc_delete_path = objname;
+	uxc_delete_replied = false;
+	req.complete_cb = uxc_delete_done_cb;
+	ubus_complete_request_async(ctx, &req);
+
+	uloop_run();
+
+	uxc_delete_path = NULL;
+	if (!uxc_delete_replied)
+		ubus_abort_request(ctx, &req);
+
+unregister:
+	ubus_unregister_event_handler(ctx, &uxc_object_remove_ev);
+out:
+	free(objname);
+}
+
 static void uxc_instance_drop(const char *name)
 {
 	static struct blob_buf req;
 	uint32_t id;
+
+	uxc_runtime_delete(name);
 
 	if (ubus_lookup_id(ctx, "container", &id))
 		return;
@@ -1595,6 +1677,7 @@ static int uxc_create(char *name, bool immediately, const char *console_socket,
 	char hostsbind[PATH_MAX];
 	char provbind[2 * PATH_MAX];
 	struct blob_attr *ptb[__PROV_MAX];
+	struct runtime_state *rsstate = NULL;
 	struct uxc_wait_state wait_state;
 
 	void *in, *ins, *j, *m;
@@ -1614,6 +1697,10 @@ static int uxc_create(char *name, bool immediately, const char *console_socket,
 
 	if (!found)
 		return -ENOENT;
+
+	rsstate = avl_find_element(&runtime, name, rsstate, avl);
+	if (rsstate && !runtime_live(rsstate))
+		uxc_instance_drop(name);
 
 	path = blobmsg_get_string(tb[CONF_PATH]);
 
@@ -2932,8 +3019,6 @@ static int uxc_delete(char *name, bool force, bool volumes)
 	const char *sfname = NULL;
 	struct stat sb;
 	const char *statevol = NULL;
-	struct uxc_wait_state wait_state;
-	char *objname = NULL;
 	bool live;
 
 	blobmsg_for_each_attr(cur, blob_data(conf.head), rem) {
@@ -2972,52 +3057,23 @@ static int uxc_delete(char *name, bool force, bool volumes)
 	}
 
 	if (rsstate) {
-		uint32_t cont_id;
-		bool have_cont_obj;
-
 		ret = ubus_lookup_id(ctx, "container", &id);
 		if (ret)
 			goto errout;
+
+		uxc_runtime_delete(rsstate->container_name);
 
 		blob_buf_init(&req, 0);
 		blobmsg_add_string(&req, "name", rsstate->container_name);
 		blobmsg_add_string(&req, "instance", rsstate->instance_name);
 
-		if (asprintf(&objname, "container.%s", rsstate->container_name) == -1) {
-			blob_buf_free(&req);
-			ret = -ENOMEM;
-			goto errout;
-		}
-
-		have_cont_obj = (ubus_lookup_id(ctx, objname, &cont_id) == 0);
-		free(objname);
-		objname = NULL;
-
-		if (have_cont_obj) {
-			memset(&wait_state, 0, sizeof(wait_state));
-			wait_state.service = rsstate->container_name;
-			wait_state.instance = rsstate->instance_name;
-			if (rsstate->runtime_pid > 0)
-				wait_state.pid = rsstate->runtime_pid;
-			if (uxc_wait_arm(&wait_state))
-				fprintf(stderr, "uxc: warning: cannot arm instance.* watcher\n");
-		}
-
 		ret = ubus_invoke(ctx, id, "delete", req.head, NULL, NULL, 3000);
 		if (ret && ret != UBUS_STATUS_NOT_FOUND) {
 			blob_buf_free(&req);
-			if (have_cont_obj)
-				uxc_wait_disarm();
 			ret = -EIO;
 			goto errout;
 		}
 
-		if (have_cont_obj && !ret &&
-		    uxc_wait_run(&wait_state, 30000) == -ETIMEDOUT)
-			fprintf(stderr, "uxc: warning: timed out waiting for container.%s removal\n",
-				rsstate->container_name);
-		if (have_cont_obj)
-			uxc_wait_disarm();
 		ret = 0;
 	}
 