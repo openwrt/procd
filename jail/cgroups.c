openwrt/procd

commit 16159bb1f70f662738395f2750a7a5e74a410408

jail: parse OCI cgroups resources
Start pure cgroup2 implementation with emulation of (some) cgroup1
properties.
Initially support converting cpu, memory, blockIO, pids to unified in
addition to directly specifying unified attributes as suggested in
https://github.com/opencontainers/runtime-spec/pull/1040

Support for converting devices and network into BPF programs is
planned.

Now that containers have their representation in the unified cgroup
hierarchy, make sure using cgroup namespaces also produces meaningful
results.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

jail/cgroups.h
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2020 Daniel Golle <daniel@makrotopia.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _JAIL_CGROUPS_H
+#define _JAIL_CGROUPS_H
+
+void cgroups_init(const char *p);
+int parseOCIlinuxcgroups(struct blob_attr *msg);
+void cgroups_apply(pid_t pid);
+void cgroups_free(void);
+
+#endif

jail/jail.c
@@ -51,6 +51,7 @@
 #include "jail.h"
 #include "log.h"
 #include "seccomp-oci.h"
+#include "cgroups.h"
 
 #include <libubox/utils.h>
 #include <libubox/blobmsg.h>
@@ -238,11 +239,11 @@ static void free_rlimits(void) {
 		free(opts.rlimits[type]);
 }
 
-static void free_opts(bool child) {
+static void free_opts(bool parent) {
 	char **tmp;
 
 	/* we need to keep argv, envp and seccomp filter in child */
-	if (child) {
+	if (parent) {
 		if (opts.ociseccomp) {
 			free(opts.ociseccomp->filter);
 			free(opts.ociseccomp);
@@ -259,6 +260,7 @@ static void free_opts(bool child) {
 			free(*(tmp++));
 
 		free(opts.envp);
+		cgroups_free();
 	};
 
 	free_rlimits();
@@ -276,6 +278,7 @@ static void free_opts(bool child) {
 	free_hooklist(opts.hooks.poststart);
 	free_hooklist(opts.hooks.poststop);
 }
+
 static int mount_overlay(char *jail_root, char *overlaydir) {
 	char *upperdir, *workdir, *optsstr, *upperetc, *upperresolvconf;
 	const char mountoptsformat[] = "lowerdir=%s,upperdir=%s,workdir=%s";
@@ -1105,6 +1108,10 @@ static int exec_jail(void *arg)
 		ERROR("parent had an error, child exiting\n");
 		return EXIT_FAILURE;
 	}
+
+	if (opts.namespace & CLONE_NEWCGROUP)
+		unshare(CLONE_NEWCGROUP);
+
 	if ((opts.namespace & CLONE_NEWUSER) || (opts.setns.user != -1)) {
 		if (setregid(0, 0) < 0) {
 			ERROR("setgid\n");
@@ -1950,33 +1957,6 @@ static int parseOCIdevices(struct blob_attr *msg)
 	return 0;
 }
 
-enum {
-	OCI_LINUX_RESOURCES,
-	OCI_LINUX_SECCOMP,
-	OCI_LINUX_SYSCTL,
-	OCI_LINUX_NAMESPACES,
-	OCI_LINUX_DEVICES,
-	OCI_LINUX_UIDMAPPINGS,
-	OCI_LINUX_GIDMAPPINGS,
-	OCI_LINUX_MASKEDPATHS,
-	OCI_LINUX_READONLYPATHS,
-	OCI_LINUX_ROOTFSPROPAGATION,
-	__OCI_LINUX_MAX,
-};
-
-static const struct blobmsg_policy oci_linux_policy[] = {
-	[OCI_LINUX_RESOURCES] = { "resources", BLOBMSG_TYPE_TABLE },
-	[OCI_LINUX_SECCOMP] = { "seccomp", BLOBMSG_TYPE_TABLE },
-	[OCI_LINUX_SYSCTL] = { "sysctl", BLOBMSG_TYPE_TABLE },
-	[OCI_LINUX_NAMESPACES] = { "namespaces", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_DEVICES] = { "devices", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_UIDMAPPINGS] = { "uidMappings", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_GIDMAPPINGS] = { "gidMappings", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_MASKEDPATHS] = { "maskedPaths", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_READONLYPATHS] = { "readonlyPaths", BLOBMSG_TYPE_ARRAY },
-	[OCI_LINUX_ROOTFSPROPAGATION] = { "rootfsPropagation", BLOBMSG_TYPE_STRING },
-};
-
 static int parseOCIsysctl(struct blob_attr *msg)
 {
 	struct blob_attr *cur;
@@ -2020,12 +2000,44 @@ static int parseOCIsysctl(struct blob_attr *msg)
 	return 0;
 }
 
+
+enum {
+	OCI_LINUX_CGROUPSPATH,
+	OCI_LINUX_RESOURCES,
+	OCI_LINUX_SECCOMP,
+	OCI_LINUX_SYSCTL,
+	OCI_LINUX_NAMESPACES,
+	OCI_LINUX_DEVICES,
+	OCI_LINUX_UIDMAPPINGS,
+	OCI_LINUX_GIDMAPPINGS,
+	OCI_LINUX_MASKEDPATHS,
+	OCI_LINUX_READONLYPATHS,
+	OCI_LINUX_ROOTFSPROPAGATION,
+	__OCI_LINUX_MAX,
+};
+
+static const struct blobmsg_policy oci_linux_policy[] = {
+	[OCI_LINUX_CGROUPSPATH] = { "cgroupsPath", BLOBMSG_TYPE_STRING },
+	[OCI_LINUX_RESOURCES] = { "resources", BLOBMSG_TYPE_TABLE },
+	[OCI_LINUX_SECCOMP] = { "seccomp", BLOBMSG_TYPE_TABLE },
+	[OCI_LINUX_SYSCTL] = { "sysctl", BLOBMSG_TYPE_TABLE },
+	[OCI_LINUX_NAMESPACES] = { "namespaces", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_DEVICES] = { "devices", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_UIDMAPPINGS] = { "uidMappings", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_GIDMAPPINGS] = { "gidMappings", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_MASKEDPATHS] = { "maskedPaths", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_READONLYPATHS] = { "readonlyPaths", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_ROOTFSPROPAGATION] = { "rootfsPropagation", BLOBMSG_TYPE_STRING },
+};
+
 static int parseOCIlinux(struct blob_attr *msg)
 {
 	struct blob_attr *tb[__OCI_LINUX_MAX];
 	struct blob_attr *cur;
 	int rem;
 	int res = 0;
+	char *cgpath;
+	char cgfullpath[256] = "/sys/fs/cgroup";
 
 	blobmsg_parse(oci_linux_policy, __OCI_LINUX_MAX, tb, blobmsg_data(msg), blobmsg_len(msg));
 
@@ -2079,6 +2091,37 @@ static int parseOCIlinux(struct blob_attr *msg)
 
 	if (tb[OCI_LINUX_DEVICES]) {
 		res = parseOCIdevices(tb[OCI_LINUX_DEVICES]);
+		if (res)
+			return res;
+	}
+
+	if (tb[OCI_LINUX_CGROUPSPATH]) {
+		cgpath = blobmsg_get_string(tb[OCI_LINUX_CGROUPSPATH]);
+		if (cgpath[0] == '/') {
+			if (strlen(cgpath) >= (sizeof(cgfullpath) - strlen(cgfullpath)))
+				return E2BIG;
+
+			strcat(cgfullpath, cgpath);
+		} else {
+			strcat(cgfullpath, "/containers/");
+			strcat(cgfullpath, opts.name); /* should be container name rather than jail name */
+			strcat(cgfullpath, "/");
+			if (strlen(cgpath) >= (sizeof(cgfullpath) - strlen(cgfullpath)))
+				return E2BIG;
+
+			strcat(cgfullpath, cgpath);
+		}
+	} else {
+		strcat(cgfullpath, "/containers/");
+		strcat(cgfullpath, opts.name); /* should be container name rather than jail name */
+		strcat(cgfullpath, "/");
+		strcat(cgfullpath, opts.name); /* should be container instance name rather than jail name */
+	}
+
+	cgroups_init(cgfullpath);
+
+	if (tb[OCI_LINUX_RESOURCES]) {
+		res = parseOCIlinuxcgroups(tb[OCI_LINUX_RESOURCES]);
 		if (res)
 			return res;
 	}
@@ -2609,7 +2652,7 @@ static void post_main(struct uloop_timeout *t)
 		}
 #endif
 
-		jail_process.pid = clone(exec_jail, child_stack + STACK_SIZE, SIGCHLD | opts.namespace, NULL);
+		jail_process.pid = clone(exec_jail, child_stack + STACK_SIZE, SIGCHLD | (opts.namespace & (~CLONE_NEWCGROUP)), NULL);
 	} else {
 		jail_process.pid = fork();
 	}
@@ -2651,6 +2694,9 @@ static void post_main(struct uloop_timeout *t)
 		}
 		close(pipes[0]);
 		set_oom_score_adj();
+
+		if (opts.ocibundle)
+			cgroups_apply(jail_process.pid);
 
 		if (opts.namespace & CLONE_NEWUSER) {
 			if (write_setgroups(jail_process.pid, true)) {

CMakeLists.txt
@@ -108,7 +108,7 @@ IF(SECCOMP_SUPPORT)
 ENDIF()
 
 IF(JAIL_SUPPORT)
-ADD_EXECUTABLE(ujail jail/jail.c jail/elf.c jail/fs.c jail/capabilities.c ${SOURCES_OCI_SECCOMP})
+ADD_EXECUTABLE(ujail jail/jail.c jail/cgroups.c jail/elf.c jail/fs.c jail/capabilities.c ${SOURCES_OCI_SECCOMP})
 TARGET_LINK_LIBRARIES(ujail ${ubox} ${ubus} ${blobmsg_json})
 INSTALL(TARGETS ujail
 	RUNTIME DESTINATION ${CMAKE_INSTALL_SBINDIR}

commit 6572718d4fcaed794ed2a432c9bfc8689e70aae8

jail: fail create when a resource limit cannot be applied
cgroups_configure() logs and moves on when a cgroup attribute cannot be
opened or written, so a bundle asking for io weights on a kernel whose
block devices have no weight interface starts with an unrestricted
share of the device it asked to be limited on. Refuse the container
instead, unless not applying the attribute could not have narrowed
anything, which is only a swap limit on a kernel built without swap.

io.max throttling does not depend on the block scheduler and is
unaffected; this is about the weight interface, which comes from BFQ or
iocost rather than from cgroup2 itself.

Fixes: 16159bb1f70f ("jail: parse OCI cgroups resources")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

jail/cgroups.c
@@ -320,15 +320,25 @@ void cgroups_create(void)
 	free(ent);
 }
 
-void cgroups_configure(void)
+/* a swap limit narrows nothing on a kernel built without swap */
+static bool cgroups_attr_vacuous(const char *key)
+{
+	if (strcmp(key, "memory.swap.max"))
+		return false;
+
+	return access("/proc/swaps", F_OK) != 0;
+}
+
+int cgroups_configure(void)
 {
 	struct cgval *valp;
-	char *ent;
 	size_t maxlen = 0;
 	int fd, dirfd;
+	int ret = 0;
+	char *ent;
 
 	if (!cgroup_path)
-		return;
+		return 0;
 
 	avl_for_each_element(&cgvals, valp, avl) {
 		size_t klen = strlen((char *)valp->avl.key);
@@ -347,14 +357,30 @@ void cgroups_configure(void)
 		snprintf(ent, maxlen, "%s/%s", cgroup_path, (char *)valp->avl.key);
 		fd = open(ent, O_WRONLY);
 		if (fd < 0) {
+			if (cgroups_attr_vacuous((char *)valp->avl.key)) {
+				INFO("%s is unavailable here, \"%s\" narrows nothing\n",
+				     ent, valp->val);
+				continue;
+			}
 			ERROR("can't open %s: %m\n", ent);
-			continue;
+			ret = ENOTSUP;
+			break;
 		}
-		if (dprintf(fd, "%s", valp->val) < 0)
+		if (dprintf(fd, "%s", valp->val) < 0) {
 			ERROR("can't write to %s: %m\n", ent);
+			close(fd);
+			if (cgroups_attr_vacuous((char *)valp->avl.key))
+				continue;
+
+			ret = ENOTSUP;
+			break;
+		}
 		close(fd);
 	}
 	free(ent);
+
+	if (ret)
+		return ret;
 
 	dirfd = open(cgroup_path, O_DIRECTORY);
 	if (dirfd < 0) {
@@ -363,13 +389,23 @@ void cgroups_configure(void)
 		attach_cgroups_ebpf(dirfd);
 		close(dirfd);
 	}
+
+	return 0;
 }
 
-void cgroups_apply(pid_t pid)
+int cgroups_apply(pid_t pid)
 {
+	int ret;
+
 	cgroups_create();
-	cgroups_configure();
+
+	ret = cgroups_configure();
+	if (ret)
+		return ret;
+
 	cgroups_attach_pid(pid);
+
+	return 0;
 }
 
 enum {

jail/cgroups.h
@@ -21,8 +21,8 @@ void cgroups_init(const char *p);
 int parseOCIlinuxcgroups(struct blob_attr *msg, bool is_update);
 void cgroups_create(void);
 void cgroups_destroy(void);
-void cgroups_configure(void);
-void cgroups_apply(pid_t pid);
+int cgroups_configure(void);
+int cgroups_apply(pid_t pid);
 int cgroups_attach_pid(pid_t pid);
 int cgroups_kill_all(void);
 int cgroups_set_frozen(bool frozen);

jail/jail.c
@@ -5422,7 +5422,9 @@ container_handle_update(struct ubus_context *ctx, struct ubus_object *obj,
 		}
 	}
 
-	cgroups_apply(jail_process.pid);
+	if (cgroups_apply(jail_process.pid))
+		return UBUS_STATUS_NOT_SUPPORTED;
+
 	return UBUS_STATUS_OK;
 }
 
@@ -7066,7 +7068,9 @@ static void post_main(struct uloop_timeout *t)
 		set_oom_score_adj();
 
 		if (opts.ocibundle) {
-			cgroups_configure();
+			if (cgroups_configure())
+				free_and_exit(ENOTSUP);
+
 			cgroups_attach_pid(jail_process.pid);
 		}
 